45effea8b06b01de14e7f3ae6e2829fc0ded7f9b
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-01 15:00:10 +0530

    Time: 0 ms (100%), Space: 53.8 MB (26.54%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -3,26 +3,35 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    // Solution using Binary Search;
+    // Binary Search Algo:
+    // Logic: An element is always considered to be strictly greater than
+    // a neighbor that is outside the array.
+    // Brute force solution requires us to perform the linear search
+    // and check the left + right element to the current element whether its 
+    // greater than its neighbors or not;
+    // But here we will try the binary search cause if the middleIdx element is
+    // greater than its next element i.e middleIdx + 1 element than we will
+    // ignore or discard the elements to the right of the middleIdx + 1
+    // else we will ignore the elements to the left of the middleIdx cause its
+    // smaller than the middleIdx;
 
     let peakElIdx = -1;
-
     let leftIdx = 0;
     let rightIdx = nums.length - 1;
-    let middleIdx = leftIdx + Math.floor((rightIdx - leftIdx) / 2);
+    let middleIdx = Math.floor(leftIdx + (rightIdx - leftIdx) / 2);
 
     while(leftIdx < rightIdx){
-        middleIdx = leftIdx + Math.floor((rightIdx - leftIdx) / 2);
+        middleIdx = Math.floor(leftIdx + (rightIdx - leftIdx) / 2);
 
-        if(nums[middleIdx] > nums[middleIdx + 1]){
-            rightIdx = middleIdx;
-        }
-        else if(nums[middleIdx] < nums[middleIdx + 1]){
+        if(nums[middleIdx + 1] > nums[middleIdx]){
             leftIdx = middleIdx + 1;
+        }
+        else{
+            rightIdx = middleIdx;
         }
     }
 
     peakElIdx = leftIdx;
-
+    
     return peakElIdx;
 };
