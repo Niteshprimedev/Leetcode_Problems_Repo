e63b335d6853c9033e000a7ba13ad09efc065447
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-02 13:02:08 +0530

    Time: 31 ms (52.94%), Space: 89.7 MB (80.39%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.js
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.js
@@ -3,61 +3,41 @@
  * @return {number}
  */
 var mostPoints = function(questions) {
+    // Bottom Up Approach:
+    const N = questions.length;
+
     let maxQuestionPoints = 0;
-    const memo = new Array(questions.length).fill(-1);
+    const memoDP = new Array(N).fill(0);
 
-    function solveOrSkip(idx, questions, memo){
-        // Base case:
-        if(idx >= questions.length) return 0;
+    for(let idxI = N - 1; idxI >= 0; idxI--){
+        const [points, brainpower] = questions[idxI];
 
-        if(memo[idx] > -1) return memo[idx];
+        let nextIdx = (idxI + brainpower + 1) >= N ? 0 : (idxI + brainpower + 1);
+        const solveItPoints = points + memoDP[nextIdx];
+        nextIdx = (idxI + 1) >= N ? 0 : idxI + 1;
+        const skipItPoints = memoDP[nextIdx];
 
-        const solveItPoints = questions[idx][0] + solveOrSkip(idx + questions[idx][1] + 1, questions, memo);
-        const leaveItPoints = solveOrSkip(idx + 1, questions, memo);
-
-        memo[idx] = Math.max(solveItPoints, leaveItPoints);
-        return memo[idx];
+        memoDP[idxI] = Math.max(solveItPoints, skipItPoints);
     }
-    return solveOrSkip(0, questions, memo);
+    // console.log(memoDP);
+    return memoDP[0];
 
     /** 
-    // Algorithm for 2 steps;
+    // Top Down Approach:
     let maxQuestionPoints = 0;
+    const memoDP[0] = new Array(N).fill(-1);
 
-    for(let idxI = 0; idxI < 1; idxI++){
-        let choiceOnePoints = 0;
-        let choiceTwoPoints = 0;
-        let choiceThreePoints = 0;
+    function solveOrSkip(idx, questions, memoDP){
+        if(idx >= questions.length) return 0;
 
-        for(let idxJ = idxI + 3; idxJ < questions.length; idxJ += 3){
-            choiceOnePoints += questions[idxJ][0];
-        }
-        for(let idxK = idxI + 4; idxK < questions.length; idxK += 4){
-            choiceTwoPoints += questions[idxK][0];
-        }
-        for(let idxL = idxI + 5; idxL < questions.length; idxL += 5){
-            choiceThreePoints += questions[idxL][0];
-        }
+        if(memoDP[idx] > -1) return memoDP[idx];
 
-        let newMaxQuestionPoints = questions[idxI][0] + choiceOnePoints;
-        maxQuestionPoints = Math.max(maxQuestionPoints, newMaxQuestionPoints);
-        
-        newMaxQuestionPoints = questions[idxI][0] + choiceTwoPoints;
-        maxQuestionPoints = Math.max(maxQuestionPoints, newMaxQuestionPoints);
+        const solveItPoints = questions[idx][0] + solveOrSkip(idx + questions[idx][1] + 1, questions, memoDP);
+        const leaveItPoints = solveOrSkip(idx + 1, questions, memoDP);
 
-        newMaxQuestionPoints = questions[idxI][0] + choiceThreePoints;
-        maxQuestionPoints = Math.max(maxQuestionPoints, newMaxQuestionPoints);
-
-        if(idxI + 1 < questions.length){
-            newMaxQuestionPoints = questions[idxI + 1][0] + choiceTwoPoints;
-            maxQuestionPoints = Math.max(maxQuestionPoints, newMaxQuestionPoints);
-        }
-        if(idxI + 2 < questions.length){
-            newMaxQuestionPoints = questions[idxI + 2][0] + choiceThreePoints;
-            maxQuestionPoints = Math.max(maxQuestionPoints, newMaxQuestionPoints);
-        }
+        memoDP[idx] = Math.max(solveItPoints, leaveItPoints);
+        return memoDP[idx];
     }
-
-    return maxQuestionPoints;
+    return solveOrSkip(0, questions, memoDP);
     */
 };
