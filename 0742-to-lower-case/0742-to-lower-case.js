a4cd8b3f72b7def0d40fc22352865b5a3f0c9cf0
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-03 17:47:34 +0530

    Time: 0 ms (100%), Space: 54.8 MB (6.18%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.js
+++ 0742-to-lower-case/0742-to-lower-case.js
@@ -8,6 +8,26 @@ var toLowerCase = function(s) {
     // Lower Case Chars ASCII Values range (97, 123);
     // Upper Case Chars ASCII Values range (65, 91);
 
+    // Better Soln:
+    let lowerCaseStrS = '';
+
+    let idxI = 0;
+    for(let char of s){
+        let lowerCaseChar = char;
+
+        const charCode = s.charCodeAt(idxI);
+        if(charCode >= 65 && charCode <= 90){
+            const lowerCaseCharCode =  charCode + 32;
+            lowerCaseChar = String.fromCharCode(lowerCaseCharCode);
+        }
+
+        lowerCaseStrS += lowerCaseChar;
+        idxI++;
+    }
+
+    return lowerCaseStrS;
+
+    /** 
     const lowerCaseAlphabets = "abcdefghijklmnopqrstuvwxyz";
     const lowerCaseCharsHashmap = new Map();
 
@@ -33,8 +53,9 @@ var toLowerCase = function(s) {
         lowerCaseStrS += lowerCaseChar;
         idxI++;
     }
-    // console.log(s.charCodeAt(1));
+    console.log(s.charCodeAt(1));
 
     return lowerCaseStrS;
     // return s.toLowerCase();
+    */
 };
