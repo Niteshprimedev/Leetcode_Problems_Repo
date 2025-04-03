4572fa28264a843d7974b0f4b1daef09cbb28194
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-03 17:44:00 +0530

    Time: 1 ms (17.53%), Space: 54.2 MB (13.55%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    // They want us to get the ASCII Values & convert them
+    // into lower case ASCII values;
+    // Lower Case Chars ASCII Values range (97, 123);
+    // Upper Case Chars ASCII Values range (65, 91);
+
+    const lowerCaseAlphabets = "abcdefghijklmnopqrstuvwxyz";
+    const lowerCaseCharsHashmap = new Map();
+
+    for(let charIdx = 0; charIdx < 26; charIdx++){
+        const hashKey = lowerCaseAlphabets[charIdx];
+        const hashValue = charIdx + 97;
+
+        lowerCaseCharsHashmap.set(hashValue, hashKey);
+    }
+
+    let lowerCaseStrS = '';
+
+    let idxI = 0;
+    for(let char of s){
+        let lowerCaseChar = char;
+
+        const charCode = s.charCodeAt(idxI);
+        if(charCode >= 65 && charCode <= 90){
+            const lowerCaseCharCode =  charCode + 32;
+            lowerCaseChar = lowerCaseCharsHashmap.get(lowerCaseCharCode);
+        }
+
+        lowerCaseStrS += lowerCaseChar;
+        idxI++;
+    }
+    // console.log(s.charCodeAt(1));
+
+    return lowerCaseStrS;
+    // return s.toLowerCase();
+};
