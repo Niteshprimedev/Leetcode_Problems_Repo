25e528cfa7e85de24f9fa2863916316ee0e86741
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-04 12:08:48 +0530

    Time: 0 ms (100%), Space: 58.2 MB (24.53%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    // Edge Case:
+    const numsArrLen = nums.length;
+    let leftIdx = 0;
+    let rightIdx = numsArrLen - 1;
+    let middleIdx = leftIdx + Math.floor((rightIdx - leftIdx) / 2);
+
+    while(leftIdx < rightIdx){
+        middleIdx = leftIdx + Math.floor((rightIdx - leftIdx) / 2);
+
+        if(nums[middleIdx] < target){
+            leftIdx = middleIdx + 1;
+        }
+        else if(nums[middleIdx] > target){
+            rightIdx = middleIdx - 1;
+        }
+        else{
+            return middleIdx;
+        }
+    }
+
+    if(nums[leftIdx] === target) return leftIdx;
+    return -1;
+};
