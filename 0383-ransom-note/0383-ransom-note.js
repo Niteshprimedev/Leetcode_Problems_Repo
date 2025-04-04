921b295a341b71f1b666906d71d757d00a0e0312
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-04 10:49:26 +0530

    Time: 11 ms (86.92%), Space: 60.6 MB (14.74%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    const ransomNoteCharsFreqHashmap = new Map();
+
+    let loopLen = ransomNote.length;
+    for(let idxI = 0; idxI < loopLen; idxI++){
+        const hashKey = ransomNote[idxI];
+        const hashValue = (ransomNoteCharsFreqHashmap.get(hashKey) || 0) + 1;
+
+        ransomNoteCharsFreqHashmap.set(hashKey, hashValue);
+    }
+
+    // console.log(ransomNoteCharsFreqHashmap);
+
+    for(let magazineChar of magazine){
+        const hashKey = magazineChar;
+
+        const isMagazineCharPresent = ransomNoteCharsFreqHashmap.has(hashKey);
+        if(isMagazineCharPresent){
+            const hashValue = ransomNoteCharsFreqHashmap.get(hashKey);
+
+            if(hashValue - 1 === 0){
+                ransomNoteCharsFreqHashmap.delete(hashKey);
+            }
+            else{
+                ransomNoteCharsFreqHashmap.set(hashKey, hashValue - 1);
+            }
+        }
+    }
+
+    // console.log(ransomNoteCharsFreqHashmap);
+    
+    const canRansomNoteBeConstructed = ransomNoteCharsFreqHashmap.size === 0 ? true : false;
+    return canRansomNoteBeConstructed;
+};
