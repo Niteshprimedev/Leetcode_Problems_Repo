693d4e29f2dc10dbac6b555fd49aaffab4762f06
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-05 10:43:22 +0530

    Time: 0 ms (100%), Space: 52.9 MB (65.54%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -18,13 +18,169 @@ var letterCombinations = function(digits) {
         "9": "wxyz",
     };
 
+    function generateCombos(currIdx, currStr, digitLetterComboArr, phoneDigiLetterObj){
+        // Base Case:
+        if(currStr.length === digits.length){
+            digitLetterComboArr.push(currStr);
+            // return digitLetterComboArr;
+            return [...digitLetterComboArr];
+        }
+
+        const digitPhoneLetters = phoneDigiLetterObj[digits[currIdx]];
+        let result = digitLetterComboArr;
+        digitPhoneLetters.split('').forEach(letter => {
+            result = generateCombos(currIdx + 1, currStr + letter, digitLetterComboArr, phoneDigiLetterObj);
+        });
+
+        // console.log(result, digitLetterComboArr, 'result');
+        return result;
+    }
+    return generateCombos(0, '', digitLetterComboArr, phoneDigitLettersObj);
+    /** 
+    // BrainStorming:::
+    const digitLetterComboArr = [];
+
+    if(digits.length === 0) return digitLetterComboArr;
+
+    const phoneDigitLettersObj = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz",
+    };
+
     let digitIdx = 0;
     const firstDigit = digits[digitIdx];
-    const  firstDigitLetters = phoneDigitLettersObj[firstDigit];
+    let  firstDigitLetters = phoneDigitLettersObj[firstDigit];
+    digitIdx++;
 
     const isDigitsLenOne = digits.length === 1 ? true : false;
+
+    if(isDigitsLenOne){
+        function generateCombos(firstIdx, firstDigiLetters){
+            // Base Case;
+            if(firstIdx === firstDigitLetters.length){
+                return digitLetterComboArr;
+            }
+
+            const letterCombo = firstDigiLetters[firstIdx];
+            digitLetterComboArr.push(letterCombo);
+
+            return generateCombos(firstIdx + 1, firstDigiLetters);
+        }
+        return generateCombos(0, firstDigitLetters);
+    }
+
+    
+    const secondDigit = digits[digitIdx];
+    let  secondDigitLetters = phoneDigitLettersObj[secondDigit];
+    digitIdx++;
+
     const isDigitsLenTwo = digits.length === 2 ? true : false;
+    if(isDigitsLenTwo){
+        function generateCombos(firstIdx, secondIdx, firstDigiLetters, secondDigiLetters){
+            // Base Case;
+            if(firstIdx === firstDigitLetters.length){
+                return digitLetterComboArr;
+            }
+
+            if(secondIdx === secondDigiLetters.length){
+                return digitLetterComboArr;
+            }
+
+            if((firstIdx > 0) && (secondIdx > 0) && firstIdx === secondIdx) return digitLetterComboArr;
+
+            const firstLetter = firstDigiLetters[firstIdx];
+            const secondLetter = secondDigiLetters[secondIdx];
+            const letterCombo = firstLetter + secondLetter;
+            digitLetterComboArr.push(letterCombo);
+
+            console.log(firstIdx, secondIdx);
+            generateCombos(firstIdx, secondIdx + 1, firstDigiLetters, secondDigiLetters);
+
+            return generateCombos(firstIdx + 1, 0, firstDigiLetters, secondDigiLetters);
+        }
+        return generateCombos(0, 0, firstDigitLetters, secondDigitLetters);
+    }
+
+    const thirdDigit = digits[digitIdx];
+    let  thirdDigitLetters = phoneDigitLettersObj[thirdDigit];
+    digitIdx++;
+
     const isDigitsLenThree = digits.length === 3 ? true : false;
+    if(isDigitsLenThree){
+        function generateCombos(firstIdx, secondIdx, thirdIdx, firstDigiLetters, secondDigiLetters, thirdDigiLetters){
+            // Base Case;
+            if(firstIdx === firstDigitLetters.length){
+                return digitLetterComboArr;
+            }
+           
+            if(secondIdx === secondDigitLetters.length){
+                return;
+            }
+
+            if(thirdIdx === thirdDigiLetters.length){
+                return;
+            }
+
+            const firstLetter = firstDigiLetters[firstIdx];
+            const secondLetter = secondDigiLetters[secondIdx];
+            const thirdLetter = thirdDigiLetters[thirdIdx];
+            const letterCombo = firstLetter + secondLetter + thirdLetter;
+
+            digitLetterComboArr.push(letterCombo);
+
+            generateCombos(firstIdx, secondIdx, thirdIdx + 1, firstDigiLetters, secondDigiLetters, thirdDigiLetters);
+
+            generateCombos(firstIdx, secondIdx + 1, thirdIdx, firstDigiLetters, secondDigiLetters, thirdDigiLetters);
+
+            return generateCombos(firstIdx + 1, secondIdx, thirdIdx, firstDigiLetters, secondDigiLetters, thirdDigiLetters);
+        }
+        return generateCombos(0, 0, 0, firstDigitLetters, secondDigitLetters, thirdDigitLetters);
+    }
+
+    const fourthDigit = digits[digitIdx];
+    let  fourthDigitLetters = phoneDigitLettersObj[fourthDigit];
+
+    function generateCombos(firstIdx, secondIdx, thirdIdx, fourthIdx, firstDigiLetters, secondDigiLetters, thirdDigiLetters, fourthDigiLetters){
+        // Base Cases;
+        if(firstIdx === firstDigiLetters.length){
+            return 
+        }
+        if(secondIdx === secondDigitLetters.length){
+            return;
+        }
+
+        if(thirdIdx === thirdDigiLetters.length){
+            return;
+        }
+
+        if(fourthIdx === fourthDigiLetters.length){
+            return;
+        }
+
+        const firstLetter = firstDigiLetters[firstIdx];
+        const secondLetter = secondDigiLetters[secondIdx];
+        const thirdLetter = thirdDigiLetters[thirdIdx];
+        const fourthLetter = fourthDigiLetters[fourthIdx];
+        const letterCombo = firstLetter + secondLetter + thirdLetter + fourthLetter;
+
+        digitLetterComboArr.push(letterCombo);
+
+        generateCombos(firstIdx, secondIdx, thirdIdx, fourthIdx + 1, firstDigiLetters, secondDigiLetters, thirdDigiLetters, fourthDigiLetters);
+
+        generateCombos(firstIdx, secondIdx, thirdIdx + 1, fourthIdx, firstDigiLetters, secondDigiLetters, thirdDigiLetters, fourthDigiLetters);
+
+        generateCombos(firstIdx, secondIdx + 1, thirdIdx, fourthIdx, firstDigiLetters, secondDigiLetters, thirdDigiLetters, fourthDigiLetters);
+
+        return generateCombos(firstIdx + 1, secondIdx, thirdIdx, fourthIdx, firstDigiLetters, secondDigiLetters, thirdDigiLetters, fourthDigiLetters);
+    }
+    return generateCombos(0, 0, 0, 0, firstDigitLetters, secondDigitLetters, thirdDigitLetters, fourthDigitLetters);
+
 
     for(let firstIdx = 0; firstIdx < firstDigitLetters.length; firstIdx++){
         if(isDigitsLenOne){
@@ -68,4 +224,5 @@ var letterCombinations = function(digits) {
     }
 
     return digitLetterComboArr;
+    */
 };
