0f62f8b916699b07b14354e713159911d55bf9d3
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-06 16:22:13 +0530

    Time: 0 ms (100%), Space: 52 MB (76.59%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -3,68 +3,41 @@
  * @return {number}
  */
 var rob = function(nums) {
+
     // Top Down Approach;
-    if(nums.length < 2){
-        return nums[0];
-    }
-
     let maxMoneyRobbery = 0;
-    const lastHouseIdx = nums.length - 1;
-    const memoDP = new Array(nums.length).fill(-1);
+    const lastHouseIdx = nums.length;
+    const memoDP = new Array(lastHouseIdx).fill(-1);
 
-    function chooseOrSkipHouse(houseIdx, houses, memoDP){
-        // console.log(houseIdx, (houseIdx < lastHouseIdx) && memoDP[houseIdx]);
+    function chooseOrSkipHouseDFS(houseIdx, houses, memoDP){
 
-        // Base Case;
-        if(houseIdx > lastHouseIdx){
+        // Base Case: if houseIndex goes out of bounds
+        if(houseIdx >= lastHouseIdx){
             return 0;
         }
 
-        // If the max robbery amount is already computed for the given house
-        // then return from the memoDP array;
-        if(memoDP[houseIdx] > -1) return memoDP[houseIdx];
+        // Return memoDP cached result if already computed
+        if(memoDP[houseIdx] !== -1) return memoDP[houseIdx];
 
-        // Else compute the max robbery amount;
-        // Return the 
-        if(houseIdx === lastHouseIdx){
-            const robberyAmount = houses[houseIdx];
-            // We need to return the robbery amount for the lastHouseIdx cause
-            // we are not making any chooseToRob or skipToRob decision for this house
-            // we are directly robbing it only when we did get to this house;
-            // Since this is the lastHouse so there's no point in skipping it cause we won't
-            // be able to alert the police by robbing the next house so just need to rob this
-            return robberyAmount;
-        }
+        // Choose to rob current house and skip next
+        const choseToRob = houses[houseIdx] + chooseOrSkipHouseDFS(houseIdx + 2, houses, memoDP);
+        // Or choose to skip current and try next
+        const skipToRob = chooseOrSkipHouseDFS(houseIdx + 1, houses, memoDP);
 
-        // The decision tree time: chose to rob right subTree;
-        const chooseToRob = houses[houseIdx] + chooseOrSkipHouse(houseIdx + 2, houses, memoDP);
-        // skip to rob left subTree;
-        const skipToRob = chooseOrSkipHouse(houseIdx + 1, houses, memoDP);
-
-        // Calculate the max robbery amount for the current house if
-        // when we chose to rob it or when we chose to skip it
-        const currMaxRobberyAmount = Math.max(chooseToRob, skipToRob);
+        // Cache the max loot from this house onward
+        const currMaxRobberyAmount = Math.max(choseToRob, skipToRob);
         memoDP[houseIdx] = currMaxRobberyAmount;
 
-        // console.log(memoDP[houseIdx], chooseToRob, skipToRob, houseIdx);
+        // console.log(memoDP[houseIdx], choseToRob, skipToRob, houseIdx);
         // Return the max robbery amount for the current house
         return memoDP[houseIdx];
     }
 
-    chooseOrSkipHouse(0, nums, memoDP);
-
-    // The robbery amount is greater in the houseIdx = 0
-    // so it means the max amount of robbery can be done starting from the house 1
-    if(memoDP[0] > memoDP[1]){
-        maxMoneyRobbery = memoDP[0];
-    }
-    // Else if the robbery amount is greater in the houseIdx = 1
-    // so it means the max amount of robbery can be done starting from the house 2
-    else if(memoDP[0] <= memoDP[1]){
-        maxMoneyRobbery = memoDP[1];
-    }
+    // Start the decision tree from the first house
+    chooseOrSkipHouseDFS(0, nums, memoDP);
 
     // console.log(memoDP);
+    maxMoneyRobbery = memoDP[0];
 
     // Return the maxAmount of Robbery that can be done without alerting the police;
     return maxMoneyRobbery;
