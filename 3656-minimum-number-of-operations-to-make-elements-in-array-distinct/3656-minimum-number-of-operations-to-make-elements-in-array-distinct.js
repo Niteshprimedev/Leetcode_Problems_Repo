54d68c55bd8c94b87281c7f564ecbffd5c3398c4
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-08 07:28:58 +0530

    Time: 3 ms (73.56%), Space: 55.5 MB (71.26%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.js
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.js
@@ -3,11 +3,12 @@
  * @return {number}
  */
 var minimumOperations = function(nums) {
+    /** 
     // Logic: Traverse through the array twice using idxI & idxJ
     // and for each repeated nums at idxI and idxJ
     // increment the minNumOfOps as well as the reset the idxI to 
     // (minNumOfOps * 3) - 1 value cause every time we remove 3 els;
-    
+
     // Brute Force Solution:
     let minNumOfOpsForDistinctArrEls = 0;
 
@@ -29,6 +30,49 @@ var minimumOperations = function(nums) {
 
         return idx - 1;
     }
+
+    return minNumOfOpsForDistinctArrEls;
+
+    T.C: O(N^2) It is N^2 - N so N^2
+    S.C: O(1)
+    */
+
+    // Logic:
+    // Using Math here, so this time the idea is to loop through the
+    // end of the array and whenever we find a repeated el, we stop there
+    // we calculate the minNumOfOps require to remove all the elements from 
+    // the starting idx 0 so we add 3 if currIdx % 3 = 0, 2 if currIdx % 3 = 1
+    // and 1 if currIdx % 3 = 2. And our answer will be currIdx + whatever value
+    // we need to add after modulo divided by 3;
+    
+    const arrLen = nums.length;
+
+    const numsElsHashmap = new Map();
+    let minNumOfOpsForDistinctArrEls = 0;
+
+    for(let currIdxI = arrLen - 1; currIdxI >= 0; currIdxI--){
+        const currNum = nums[currIdxI];
+
+        const isNumElRepeated = numsElsHashmap.has(currNum);
+        if(isNumElRepeated){
+            if(currIdxI % 3 === 0){
+                minNumOfOpsForDistinctArrEls += (currIdxI + 3) / 3;
+            }
+            else if(currIdxI % 3 === 1){
+                minNumOfOpsForDistinctArrEls += (currIdxI + 2) / 3;
+            }
+            else if(currIdxI % 3 === 2){
+                minNumOfOpsForDistinctArrEls += (currIdxI + 1) / 3;
+            }
+            break;
+        }
+        else{
+            numsElsHashmap.set(currNum, currIdxI);
+        }
+    }
+
+    // console.log(numsElsHashmap);
+    numsElsHashmap.clear();
 
     return minNumOfOpsForDistinctArrEls;
 };
