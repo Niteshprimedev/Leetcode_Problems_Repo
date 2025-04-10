3b753e2888fa894860cd74fd4cdc966928419ca4
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-10 18:09:32 +0530

    Time: 0 ms (100%), Space: 17.7 MB (50.52%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -1,10 +1,30 @@
 class Solution:
     def invertTree(self, root):
+        # Logic:
+        # Step1: If the current node has no left child, swap the left and right subtree, move to left
+        # I said to left (cause it was originally the right)
+        # Step2: If the current node has left child, then before moving to the left child, make a connection
+        # And here's the trick, we check the left child is connected to the current_node or not cause
+        # once we make the thread connection then our right child which was threaded to the current_node for backtracking
+        # is actually has become our left child now
+        # If the prev_node is connected to the current_node,
+        # then remove the connection, swap the left and right subtrees, and move to left child
+        # (originally it was right child) 
+        # If the prev_node is not connected to the current_node, then find its rightmost child
+        # connect it to the current_node and then move to the left (actually, this is original left only)
+        
         current_node = root
 
         while current_node is not None: # meaning that the tree traversal is not done
-            if current_node.left is not None:
-                # Find the rightmost node of the left subtree
+            # No left child; just swap and go left
+            if current_node.left is None:
+                # Swap the nodes now
+                swap_node = current_node.left
+                current_node.left = current_node.right
+                current_node.right = swap_node
+                current_node = current_node.left # Continue to what was originally the right
+
+            else:
                 prev_node = current_node.left
                 while prev_node.left is not None and prev_node.left != current_node:
                     # while prev node is not None and prev node is not the threaded node
@@ -26,13 +46,5 @@ class Solution:
                         prev_node = prev_node.right
                     prev_node.right = current_node
                     current_node = current_node.left
-                
-                # No left child; just swap and go left
-            else:
-                # Swap the nodes now
-                swap_node = current_node.left
-                current_node.left = current_node.right
-                current_node.right = swap_node
-                current_node = current_node.left # Continue to what was originally the right
                 
         return root
