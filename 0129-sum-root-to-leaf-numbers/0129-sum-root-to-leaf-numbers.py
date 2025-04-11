d51d2ef9270f6f6b65996437d2a303ffb77ee241
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-11 16:07:28 +0530

    Time: 0 ms (100%), Space: 17.9 MB (35.66%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -29,42 +29,32 @@ class Solution:
 
         return total_sum
         '''
+        # Edge case: empty tree
+        if not root:
+            return 0
 
-        # Morris Traversal:
         total_sum = 0
-        curr_sum = 0
-        depth = 0
-
         current_node = root
 
-        while(current_node is not None):
-            if current_node.left is None:
-                curr_sum = (curr_sum * 10) + current_node.val
+        # Initialize the stack with tuple: (node, current path sum)
+        stack = deque([(current_node, 0)])
+        # print('stack', stack)
 
-                # Found Leaf Node on the rightSubTree & it doesn't have left child
-                if current_node.right is None:
-                    total_sum += curr_sum
-                
-                current_node = current_node.right
-            else:
-                prev_node = current_node.left
-                depth = 1
-                while (prev_node.right is not None) and (prev_node.right != current_node):
-                    prev_node = prev_node.right
-                    depth = depth + 1
-                
-                if prev_node.right is None:
-                    prev_node.right = current_node
-                    curr_sum = (curr_sum * 10) + current_node.val
-                    current_node = current_node.left
-                else:
-                    prev_node.right = None
-                    # Found Leaf Node on the leftSubTree & it doesn't have right child
-                    if prev_node.left is None:
-                        total_sum += curr_sum
-                        
-                    # Backtrack to the root node, and reset the curr_sum
-                    curr_sum = curr_sum // (10 ** depth)
-                    current_node = current_node.right
+        # Iterative DFS Preorder Traversal
+        while(len(stack)):
+            current_node, curr_path_sum = stack.pop()
+            # Build the number by adding the current digit
+            curr_path_sum = (curr_path_sum * 10) + current_node.val
+
+            # If it's a leaf node, add the path sum to total
+            if(current_node.left is None and current_node.right is None):
+                total_sum += curr_path_sum
+
+            # Push right child first so left gets processed first (preorder style)
+            if(current_node.right is not None):
+                stack.append((current_node.right, curr_path_sum))
+
+            if(current_node.left is not None):
+                stack.append((current_node.left, curr_path_sum))
             
         return total_sum
