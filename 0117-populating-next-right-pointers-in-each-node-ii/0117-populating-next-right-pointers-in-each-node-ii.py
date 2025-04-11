aab45f11d15031ecd09b6ad83dc7c687b3019b28
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-11 21:36:58 +0530

    Time: 44 ms (72.98%), Space: 18.8 MB (81.83%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -14,32 +14,32 @@ class Solution:
             return root
         
          # Starting at root level
-        last_level_head = root
+        prev_level_root = root
 
         while True:
             next_level_head = None # Will point to first node of next level
             next_level_tail = None # Will keep linking nodes at next level
 
             # Traverse current level using .next pointers
-            while last_level_head:
+            while prev_level_root:
                 # Handle left child
-                if last_level_head.left is not None:
+                if prev_level_root.left is not None:
                     if next_level_tail is None:
-                        next_level_head = last_level_head.left
-                        next_level_tail = last_level_head.left
+                        next_level_head = prev_level_root.left
+                        next_level_tail = prev_level_root.left
                     else:
-                        next_level_tail.next = last_level_head.left
+                        next_level_tail.next = prev_level_root.left
                         next_level_tail = next_level_tail.next
                 # Handle right child
-                if last_level_head.right is not None:
+                if prev_level_root.right is not None:
                     if next_level_tail is None:
-                        next_level_head = last_level_head.right
-                        next_level_tail = last_level_head.right
+                        next_level_head = prev_level_root.right
+                        next_level_tail = prev_level_root.right
                     else:
-                        next_level_tail.next = last_level_head.right
+                        next_level_tail.next = prev_level_root.right
                         next_level_tail = next_level_tail.next
                 # Move sideways in current level
-                last_level_head = last_level_head.next
+                prev_level_root = prev_level_root.next
 
             # No more levels to process
             if next_level_head is None:
@@ -47,6 +47,6 @@ class Solution:
 
             # Traverse to next level this is to process the 
             # next left child and right child nodes
-            last_level_head = next_level_head
+            prev_level_root = next_level_head
         
         return root
