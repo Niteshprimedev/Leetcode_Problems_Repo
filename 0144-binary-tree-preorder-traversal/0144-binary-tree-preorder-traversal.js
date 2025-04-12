bfaa41270ad0a70c72c044b6fcef5949bfca0c5e
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-12 17:35:38 +0530

    Time: 0 ms (100%), Space: 53.3 MB (71.07%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
@@ -11,40 +11,26 @@
  * @return {number[]}
  */
 var preorderTraversal = function(root) {
-    // Morris Preorder Traversal
-    // Logic: Root -> Left -> Right
-    // Case1: If the left node is null then print left & go to right
-    // Case2: If the left node is not null, then before going to the leftSubTree,
-    // make a thread connection to the current node from the right most node of the
-    // leftSubTree and then go left
-    // Case3: If the thread is already connected, then remove the thread connection
-
+    // Iterative Preorder;
     const preOrderTraversal = [];
+    const rightLeftNodesStack = [];
     let currentNode = root;
 
-    while(currentNode !== null){
-        // Case1: If the leftNode is null, then print root & go right
-        if(currentNode.left === null){
-            preOrderTraversal.push(currentNode.val);
-            currentNode = currentNode.right;
+    while(currentNode){
+        preOrderTraversal.push(currentNode.val);
+
+        if(currentNode.right !== null){
+            rightLeftNodesStack.push(currentNode.right);
         }
-        // Case2: If the leftNode is not null, then make a thread connnection first
+        if(currentNode.left !== null){
+            rightLeftNodesStack.push(currentNode.left);
+        }
+        
+        if(rightLeftNodesStack.length > 0){
+            currentNode = rightLeftNodesStack.pop();
+        }
         else{
-            let prevRootNode = currentNode.left;
-            while(prevRootNode.right !== null && prevRootNode.right !== currentNode){
-                prevRootNode = prevRootNode.right;
-            }
-            // If the thread connection is not connected to currentNode then connect it;
-            if(prevRootNode.right === null){
-                prevRootNode.right = currentNode;
-                preOrderTraversal.push(currentNode.val);
-                currentNode = currentNode.left;
-            }
-            // If the thread is already connected then remove it;
-            else{
-                prevRootNode.right = null;
-                currentNode = currentNode.right;
-            }
+            currentNode = currentNode.left;
         }
     }
 
