80f072faae2c687a13472ef55c1688800d605881
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-12 16:38:03 +0530

    Time: 72 ms (12.55%), Space: 64.7 MB (64.29%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
@@ -12,47 +12,92 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    let lcaNode = [null];
-    let isPthNodeFound = false;
-    let iQthNodeFound = false;
+    // Logic used from Max_PATH_SUM Problem
+    // where we return the path sum to its root/parent node
+    // Similarly, we are checking the LCA on the currentNode and leftSubTree
+    // plus rightSubTree level
+    // Three Conditions: The root is the lca which is having the p node on the 
+    // leftSubTree and q node on rightSubTree
+    // The node is the lca which is having the p or q node on the leftSubTree
+    // The node is the lca which is having the p or q node on the rightSubTree
 
-    function traverse(currentNode, lcaNode){
-        // Base Case:
+    let lcaNode = null;
+    let isPthNodeFound = false;
+    let isQthNodeFound = false;
+
+    // If p is null & q exist then return q as LCA
+    if(p === null && q !== null){
+        lcaNode = q;
+        return lcaNode;
+    }
+    // If q is null & p exist then return p as LCA
+    else if(p !== null && q === null){
+        lcaNode = p;
+        return lcaNode;
+    }
+
+    // console.log(lcaNode);
+
+    function traverse(currentNode){
         if(currentNode === null){
             return false;
         }
 
-        const isNodeOnLeftSubtree = traverse(currentNode.left, lcaNode);
-        const isNodeOnRightSubtree = traverse(currentNode.right, lcaNode);
+        if(lcaNode !== null){
+            return false;
+        }
 
-        let isCurrentNodePorQ = false;
+        // Leaf node condition;
+        if(currentNode.left === null && currentNode.right === null){
+            // When both the p and q are null;
+            if(p === null && q === null){
+                lcaNode = currentNode;
+                return lcaNode;
+            }
+        }
+
+        const isNodeOnLeftTree = traverse(currentNode.left);
+        const isNodeOnRightTree = traverse(currentNode.right);
+
+        let isNodeCurrentNode  = false;
+
         if(currentNode === p){
-            isCurrentNodePorQ = true;
+            isPthNodeFound = true;
+            isNodeCurrentNode = true;
         }
         if(currentNode === q){
-            isCurrentNodePorQ = true;
+            isQthNodeFound = true;
+            isNodeCurrentNode = true;
         }
 
-        const isBothNodesFound = isNodeOnLeftSubtree && isNodeOnRightSubtree;
-        const isLeftNodeAndCurrentNode = isCurrentNodePorQ && isNodeOnLeftSubtree;
-        const isRightNodeAndCurrentNode = isCurrentNodePorQ && isNodeOnRightSubtree;
+        const isBothPAndQNodesFound = isPthNodeFound && isQthNodeFound;
+        const isNodeOnBothLeftAndRightTree = isNodeOnLeftTree && isNodeOnRightTree;
 
-        if(isBothNodesFound){
-            lcaNode[0] = currentNode;
-            return false;
+        // THE COMMON ANCESTOR OF P & Q NODES on the root/parent level
+        if (isBothPAndQNodesFound && isNodeOnBothLeftAndRightTree){
+            lcaNode = currentNode;
+            isPthNodeFound = false;
+            isQthNodeFound = false;
         }
-        else if(isLeftNodeAndCurrentNode|| isRightNodeAndCurrentNode){
-            lcaNode[0] = currentNode;
-            return false;
+        // THE COMMON ANCESTOR OF P & Q NODES is the current Node with having
+        // P OR Q Node as its descendant on the Left Sub Tree
+        else if(isBothPAndQNodesFound && (isNodeOnLeftTree && isNodeCurrentNode)){
+            lcaNode = currentNode;
+            isPthNodeFound = false;
+            isQthNodeFound = false;
+        }
+        // THE COMMON ANCESTOR OF P & Q NODES is the current Node with having
+        // P OR Q Node as its descendant on the Right Sub Tree
+        else if(isBothPAndQNodesFound && (isNodeOnRightTree && isNodeCurrentNode)){
+            lcaNode = currentNode;
+            isPthNodeFound = false;
+            isQthNodeFound = false;
         }
 
-        if(currentNode === p || currentNode === q){
-            return true;
-        }
-
-        return isCurrentNodePorQ || isNodeOnLeftSubtree || isNodeOnRightSubtree;
+        return isNodeCurrentNode || isNodeOnLeftTree || isNodeOnRightTree;
     }
-    traverse(root, lcaNode);
 
-    return lcaNode[0];
+    traverse(root);
+
+    return lcaNode;
 };
