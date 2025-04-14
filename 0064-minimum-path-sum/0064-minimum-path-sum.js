51cae61cb2d18df13a672f19fb800e443f1ca797
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-14 15:26:35 +0530

    Time: 11 ms (28.19%), Space: 58.1 MB (35.71%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.js
+++ 0064-minimum-path-sum/0064-minimum-path-sum.js
@@ -3,51 +3,7 @@
  * @return {number}
  */
 var minPathSum = function(grid) {
-    /** 
-    // Logic: Recurrence Relation:
-    // Step1: Express the problem in terms of indices
-    // Step2: Do all the stuff on that index;
-    // Step3: Return the min of topPathSum and leftPathSum;
-
-    const mRows = grid.length - 1;
-    const nCols = grid[0].length - 1;
-    let minPathSum = Infinity;
-
-    const memoDP = new Array(mRows + 1).fill(0).map(() => new Array(nCols + 1).fill(-1));
-
-    function allPathsSum(mRows, nCols){
-
-
-        // Base Case:
-        if(mRows === 0 && nCols === 0){
-
-            return grid[mRows][nCols];
-        }
-
-        // If we go beyond the left & top boundary:
-        // Then it's an invalid path;
-        if(mRows < 0 || nCols < 0){
-            return Infinity;
-        }
-
-        if(memoDP[mRows][nCols] !== -1){
-            return memoDP[mRows][nCols];
-        }
-
-        const topPathSum =  grid[mRows][nCols] + allPathsSum(mRows, nCols - 1);
-        const leftPathSum = grid[mRows][nCols] + allPathsSum(mRows - 1, nCols);
-
-        memoDP[mRows][nCols] = Math.min(topPathSum, leftPathSum);
-
-        return memoDP[mRows][nCols];
-    }
-    minPathSum = allPathsSum(mRows, nCols);
-
-    // console.log(memoDP, memoDP[0][0])
-
-    return minPathSum;
-    */
-
+    
     // Bottom Up Approach:
     // To convert TOP DOWN TO BOTTOM UP:
     // You need to follow below steps:
@@ -55,19 +11,23 @@ var minPathSum = function(grid) {
     // 2) Express all states in for loops
     // 3) Copy the recurrence and write it in terms of DP;
 
+    // SPACE OPTIMIZATION; when you don't need to carry entire DP array but
+    // just the prevRow of the memoDP; so carry that only to optimize space;
+
     const mRows = grid.length;
     const nCols = grid[0].length;
 
     // Initialize DP table
-    const memoDP = new Array(mRows).fill(0).map(() => new Array(nCols).fill(-1));
-
-    // The memo Base Case for Tabulation;
-    memoDP[0][0] = grid[0][0];
+    let prevDP = new Array(nCols).fill(Infinity);
 
     for(let rowIdx = 0; rowIdx < mRows; rowIdx++){
+
+        // The memo Base Case for Tabulation;
+        const currDP = new Array(nCols).fill(0);
+
         for(let colIdx = 0; colIdx < nCols; colIdx++){
             if(rowIdx === 0 && colIdx === 0){
-                memoDP[rowIdx][colIdx] = grid[0][0];
+                currDP[colIdx] = grid[0][0];
             }
             else{
 
@@ -82,22 +42,23 @@ var minPathSum = function(grid) {
                 // Calculate the topPathsum by getting the top row value and the current
                 // cell value
                 if(rowIdx > 0){
-                    topPathSum = grid[rowIdx][colIdx] + memoDP[rowIdx - 1][colIdx];
+                    topPathSum = grid[rowIdx][colIdx] + prevDP[colIdx];
                 }
                 // Calculate the leftPathsum by getting the left col value and the current
                 // cell value
                 if(colIdx > 0){
-                    leftPathSum = grid[rowIdx][colIdx] + memoDP[rowIdx][colIdx - 1];
+                    leftPathSum = grid[rowIdx][colIdx] + currDP[colIdx - 1];
                 }
 
                 // Minimum of the two paths: How do we get to the minimum path sum 
                 // for the current cell: by checking the min of topPath and leftPath
-                memoDP[rowIdx][colIdx] = Math.min(topPathSum, leftPathSum);
+                currDP[colIdx] = Math.min(topPathSum, leftPathSum);
             }
-
         }
+
+        prevDP = currDP;
     }
 
-    // console.log(memoDP);
-    return memoDP[mRows - 1][nCols - 1];
+    // console.log(prevDP);
+    return prevDP[nCols - 1];
 };
