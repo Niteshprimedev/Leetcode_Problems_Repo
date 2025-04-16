3508ee16736958a6f0dea9690f8e94eefa6b0596
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-16 20:42:52 +0530

    Time: 97 ms (23%), Space: 59.9 MB (56.11%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -48,4 +48,147 @@ var coinChange = function(coins, amount) {
     minNumOfCoins = allCoinsSumDFS(totalCoins - 1, amount);
 
     return minNumOfCoins === Infinity ? -1 : minNumOfCoins;
+
+        /**
+    if(amount === 0) return 0;
+
+    const totalCoins = coins.length;
+    let minNumOfCoins;
+
+    function allCoinsSumDFS(currIdx, target){
+        // Base Case:
+        if(currIdx === 0){
+            if (target % coins[currIdx] === 0){
+                return target / coins[currIdx];
+            }
+            return Infinity;
+        }
+
+        const notPick = allCoinsSumDFS(currIdx - 1, target);
+        let pick = Infinity;
+
+        if(coins[currIdx] <= target){
+            pick = 1 + allCoinsSumDFS(currIdx, target - coins[currIdx]);
+        }
+
+        return Math.min(pick, notPick);
+    }
+    minNumOfCoins = allCoinsSumDFS(totalCoins - 1, amount);
+    return minNumOfCoins === Infinity ?  -1 : minNumOfCoins;
+    */
+    
+    /**
+    // Top Down Approach:
+    if(amount === 0) return 0;
+
+    const totalCoins = coins.length;
+    let minNumOfCoins;
+    const memoDP = new Array(amount + 1).fill(0);
+
+    function allCoinsSumDFS(currAmountSum){
+        // Base Case:
+        if(currAmountSum === amount){
+            return 0;
+        }
+
+        if(currAmountSum > amount){
+            return Infinity;
+        }
+
+        if(memoDP[currAmountSum] !== 0){
+            return memoDP[currAmountSum];
+        }
+
+        let minCoinsCount = Infinity;
+
+        // dfs(C1) -> dfs(C2), dfs(C4), dfs(C6)
+        for(let idxI = 0; idxI < coins.length; idxI++){
+            const currCoin = coins[idxI];
+            const newCount = 1 + allCoinsSumDFS(currAmountSum + currCoin);
+            minCoinsCount = Math.min(minCoinsCount, newCount);
+        }
+
+        memoDP[currAmountSum] = minCoinsCount;
+        return memoDP[currAmountSum];
+    }
+    minNumOfCoins = allCoinsSumDFS(0);
+
+    // console.log(memoDP);
+    return minNumOfCoins === Infinity ?  -1 : minNumOfCoins;
+    
+    */
+
+    /** 
+    // Bottom Up Approach:
+    // Using Striver's Top To Bottom Technique;
+    let minNumOfCoins = Infinity;
+    const memoDP = new Array(amount + 1).fill(Infinity);
+
+    // Base Case:
+    for(let amountIdx = 0; amountIdx <= amount; amountIdx++){
+        if(amountIdx === amount){
+            memoDP[amountIdx] = 0;
+        }
+        else{
+            memoDP[amountIdx] = Infinity;
+        }
+    }
+
+    memoDP[0] = 0;
+    // console.log(memoDP);
+
+    for(let amountIdx = 1; amountIdx <= amount; amountIdx++){
+
+        let minCoinsCount = Infinity;
+        for(let coin of coins){
+            let newCount = Infinity;
+
+            if(amountIdx - coin >= 0){
+                newCount = 1 + memoDP[amountIdx - coin];
+            }
+            minCoinsCount = Math.min(minCoinsCount, newCount);
+        }
+
+        memoDP[amountIdx] = minCoinsCount;
+    }
+
+    // console.log(memoDP);
+    minNumOfCoins = memoDP[amount];
+
+    return minNumOfCoins === Infinity ? -1 : minNumOfCoins;
+
+    */
+    
+    /** 
+    // Work in Progresss on April 16th 2025:
+    // Space Optimized Bottom Up Approach:
+    let minNumOfCoins = Infinity;
+    let prevDP = [0];
+
+    // console.log(prevDP);
+
+    for(let amountIdx = 1; amountIdx <= amount; amountIdx++){
+        const currDP = new Array(amountIdx _+ 1).fill(Infinity);
+
+        let minCoinsCount = Infinity;
+
+        for(let coin of coins){
+            let newCount = Infinity;
+
+            if(amountIdx - coin >= 0){
+                newCount = 1 + prevDP[amountIdx - coin];
+            }
+            minCoinsCount = Math.min(minCoinsCount, newCount);
+            currDP[]
+        }
+
+        currDP[amountIdx] = minCoinsCount;
+        prevDP = currDP;
+    }
+
+    // console.log(memoDP);
+    minNumOfCoins = prevDP[0];
+
+    return minNumOfCoins === Infinity ? -1 : minNumOfCoins;
+    */
 };
