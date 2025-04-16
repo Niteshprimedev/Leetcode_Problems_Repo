6f06bfbaf260df430b8261b05aa15d2e53cf03a0
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-16 20:42:17 +0530

    Time: 97 ms (23%), Space: 61.7 MB (27.88%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -4,139 +4,48 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
-    /**
-    if(amount === 0) return 0;
+   
+    // Striver's Version: Using Pick and Not Pick Technique:
 
+    let minNumOfCoins = Infinity;
     const totalCoins = coins.length;
-    let minNumOfCoins;
+    const memoDP = new Array(totalCoins).fill(0).map(() => new Array(amount + 1).fill(-1));
 
-    function allCoinsSumDFS(currIdx, target){
+    function allCoinsSumDFS(currIdx, currAmount){
         // Base Case:
         if(currIdx === 0){
-            if (target % coins[currIdx] === 0){
-                return target / coins[currIdx];
+            const firstCoin = coins[currIdx];
+
+            const isFirstCoinCanTaken = currAmount % firstCoin === 0 ? true : false;
+            if(isFirstCoinCanTaken){
+                // how many denominations?
+                const coinsDenominations = currAmount / firstCoin;
+                return coinsDenominations;
             }
-            return Infinity;
+            else{
+                return Infinity;
+            }
         }
 
-        const notPick = allCoinsSumDFS(currIdx - 1, target);
-        let pick = Infinity;
-
-        if(coins[currIdx] <= target){
-            pick = 1 + allCoinsSumDFS(currIdx, target - coins[currIdx]);
+        // If the current coin path is already calculated then return it;
+        if(memoDP[currIdx][currAmount] !== -1){
+            return memoDP[currIdx][currAmount];
         }
 
-        return Math.min(pick, notPick);
+        // Pick Case:
+        let pickCurrCoinCounts = Infinity;
+        if(coins[currIdx] <= currAmount){
+            pickCurrCoinCounts = 1 + allCoinsSumDFS(currIdx, currAmount - coins[currIdx]);
+        }
+
+        // Not Pick Case:
+        const skipCurrCoinCounts = 0 + allCoinsSumDFS(currIdx - 1, currAmount);
+
+        memoDP[currIdx][currAmount] = Math.min(pickCurrCoinCounts, skipCurrCoinCounts);
+        // console.log(memoDP);
+        return  memoDP[currIdx][currAmount];
     }
     minNumOfCoins = allCoinsSumDFS(totalCoins - 1, amount);
-    return minNumOfCoins === Infinity ?  -1 : minNumOfCoins;
-    */
-    
-    /**
-    // Top Down Approach:
-    if(amount === 0) return 0;
-
-    const totalCoins = coins.length;
-    let minNumOfCoins;
-    const memoDP = new Array(amount + 1).fill(0);
-
-    function allCoinsSumDFS(currAmountSum){
-        // Base Case:
-        if(currAmountSum === amount){
-            return 0;
-        }
-
-        if(currAmountSum > amount){
-            return Infinity;
-        }
-
-        if(memoDP[currAmountSum] !== 0){
-            return memoDP[currAmountSum];
-        }
-
-        let minCoinsCount = Infinity;
-
-        // dfs(C1) -> dfs(C2), dfs(C4), dfs(C6)
-        for(let idxI = 0; idxI < coins.length; idxI++){
-            const currCoin = coins[idxI];
-            const newCount = 1 + allCoinsSumDFS(currAmountSum + currCoin);
-            minCoinsCount = Math.min(minCoinsCount, newCount);
-        }
-
-        memoDP[currAmountSum] = minCoinsCount;
-        return memoDP[currAmountSum];
-    }
-    minNumOfCoins = allCoinsSumDFS(0);
-
-    // console.log(memoDP);
-    return minNumOfCoins === Infinity ?  -1 : minNumOfCoins;
-    
-    */
-
-    // Bottom Up Approach:
-    let minNumOfCoins = Infinity;
-    const memoDP = new Array(amount + 1).fill(Infinity);
-
-    // Base Case:
-    for(let amountIdx = 0; amountIdx <= amount; amountIdx++){
-        if(amountIdx === amount){
-            memoDP[amountIdx] = 0;
-        }
-        else{
-            memoDP[amountIdx] = Infinity;
-        }
-    }
-
-    memoDP[0] = 0;
-    // console.log(memoDP);
-
-    for(let amountIdx = 1; amountIdx <= amount; amountIdx++){
-
-        let minCoinsCount = Infinity;
-        for(let coin of coins){
-            let newCount = Infinity;
-
-            if(amountIdx - coin >= 0){
-                newCount = 1 + memoDP[amountIdx - coin];
-            }
-            minCoinsCount = Math.min(minCoinsCount, newCount);
-        }
-
-        memoDP[amountIdx] = minCoinsCount;
-    }
-
-    // console.log(memoDP);
-    minNumOfCoins = memoDP[amount];
 
     return minNumOfCoins === Infinity ? -1 : minNumOfCoins;
-    
-    /**
-    // Bottom Up Approach:
-    // Chat GPT Version:
-    if(amount === 0) return 0;
-
-    const totalCoins = coins.length;
-    let minNumOfCoins;
-    
-    // Base Case:
-    // The minimum Amount is Infinity;
-    const memoDP = new Array(amount + 1).fill(Infinity);
-    memoDP[0] = 0; // It takes 0 coins to make 0 amount
-
-    for(let amountIdx = 1; amountIdx <= amount; amountIdx++){
-
-        for(let coin of coins){
-
-            // We can only make a amount using the coins smaller
-            // than or equal to my current amount:
-            // For amount 3, I can't use coin 5 right? because it will never be possible;
-            if(amountIdx - coin >= 0){
-                memoDP[amountIdx] = Math.min(memoDP[amountIdx], 1 + memoDP[amountIdx - coin]);
-            }
-        }
-    }
-
-    minNumOfCoins = memoDP[amount];
-    return minNumOfCoins === Infinity ? -1 : minNumOfCoins;
-    */
 };
