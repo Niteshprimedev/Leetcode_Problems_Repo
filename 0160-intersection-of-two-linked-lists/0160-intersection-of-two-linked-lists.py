2c2087d25caf4ffa9362997b0812cd278edc3d0c
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-16 09:49:46 +0530

    Time: 88 ms (41.57%), Space: 27.8 MB (70.1%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -12,10 +12,13 @@ class Solution:
         current_nodeA = headA
         current_nodeB = headB
 
+        # Traverse both lists
         while current_nodeA != current_nodeB:
+            # Switch to other head when end is reached
             current_nodeA = current_nodeA.next if current_nodeA is not None else headB
             current_nodeB =  current_nodeB.next if current_nodeB is not None else headA
         
+        # Either the intersection node or None
         intersect_node = current_nodeB
         return intersect_node
 
