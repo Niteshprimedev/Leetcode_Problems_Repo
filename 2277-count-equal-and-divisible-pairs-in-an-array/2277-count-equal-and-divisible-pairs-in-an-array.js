2b8904634954dbf6bf84c2ab440b14a6c5329f70
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-17 16:59:19 +0530

    Time: 12 ms (5.68%), Space: 60.1 MB (7.86%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.js
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.js
@@ -4,6 +4,7 @@
  * @return {number}
  */
 var countPairs = function(nums, k) {
+    /**
     // Brute Force Solution;
     let countArrPairs = 0;
     for(let idxI = 0; idxI < nums.length; idxI++)  {
@@ -17,5 +18,81 @@ var countPairs = function(nums, k) {
             }
         }
     }
+    return countArrPairs;
+
+    */
+
+    let countArrPairs = 0;
+    function calcGCD(num1, num2){
+        while(num2 !== 0){
+            let temp = num2;
+            num2 = num1 % num2;
+            num1 = temp;
+        }
+
+        return num1;
+    }
+
+    const numsElsHashmap = new Map();
+
+    for(let idxI = 0; idxI < nums.length; idxI++){
+        const currNum = nums[idxI];
+        const hashKey = currNum;
+
+        let hashValue = new Map();
+
+        const isHashKeyNotPresent = numsElsHashmap.has(hashKey) !== true;
+        if(isHashKeyNotPresent){
+            numsElsHashmap.set(hashKey, hashValue);
+        }
+
+        hashValue = numsElsHashmap.get(hashKey);
+        hashValue.set(idxI, hashKey);
+
+        numsElsHashmap.set(hashKey, hashValue);
+    }
+
+    // console.log(numsElsHashmap, calcGCD(2, 0));
+
+    // for(let [key, value] of numsElsHashmap){
+    //     const indicesHashValuesMap = value;
+
+    //     for(let [idxKey, idxValue] of indicesHashValuesMap){
+    //         const gdcVal = calcGCD(idxKey, k);
+
+    //         let idxJ = k / gdcVal;
+    //         let tableIdx = 1;
+
+    //         for(idxJ; idxJ < nums.length; idxJ *= tableIdx){
+
+    //             const isValuePresent = idxValue.has(idxJ);
+    //             if(isValuePresent && idxJ > idxKey){
+    //                 const value = idxValue.get(idxJ);
+    //             }  
+    //             tableIdx += 1
+    //         }
+    //     }
+    // }
+
+    for(let numIdx = 0; numIdx < nums.length; numIdx++){
+        const currNum = nums[numIdx];
+        const gcdVal = calcGCD(numIdx, k);
+
+        const hashValue = numsElsHashmap.get(currNum);
+        let idxJ = k / gcdVal;
+
+        // console.log(idxJ, gcdVal);
+
+        for(let [key, value] of hashValue){
+            if(key !== numIdx && key % idxJ === 0){
+                countArrPairs += 1;
+            }
+        }
+
+        hashValue.delete(numIdx);
+        numsElsHashmap.set(currNum, hashValue);
+
+    }
+
     return countArrPairs;
 };
