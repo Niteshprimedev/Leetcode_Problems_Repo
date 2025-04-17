b7534b916892f732d101db83479a3c71bab3d643
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-17 14:11:08 +0530

    Time: 26 ms (46.63%), Space: 66.5 MB (25%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.js
@@ -0,0 +1,111 @@
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+var maximalSquare = function(matrix) {
+    // SAME Logic as finding the maximum rectangle area in a histogram
+    // Just need to make sure that the height and the width are same;
+
+    function largestSqArea(sqHeights){
+        let maxSqArea = 0;
+        const heightsLen = sqHeights.length;
+
+        const prefixLeft = [];
+        let suffixRight = [];
+
+        let prevSmallerElStack = []
+
+        for(let idxI = 0; idxI < heightsLen; idxI++){
+            const currHeight = sqHeights[idxI];
+
+            if(prevSmallerElStack.length > 0){
+                let prevIdx = prevSmallerElStack.pop();
+                while(sqHeights[prevIdx] >= currHeight && prevSmallerElStack.length > 0){
+                    prevIdx = prevSmallerElStack.pop();
+                }
+
+                if(sqHeights[prevIdx] < currHeight){
+                    prefixLeft.push(prevIdx + 1);
+                    prevSmallerElStack.push(prevIdx);
+                }
+                else{
+                    prefixLeft.push(0);
+                }
+            }
+            else{
+                prefixLeft.push(0);
+            }
+
+            prevSmallerElStack.push(idxI);
+        }
+
+        prevSmallerElStack = [];
+
+        for(let idxI = heightsLen - 1; idxI >= 0; idxI--){
+            const currHeight = sqHeights[idxI];
+
+            if(prevSmallerElStack.length > 0){
+                let prevIdx = prevSmallerElStack.pop();
+                while(sqHeights[prevIdx] >= currHeight && prevSmallerElStack.length > 0){
+                    prevIdx = prevSmallerElStack.pop();
+                }
+
+                if(sqHeights[prevIdx] < currHeight){
+                    suffixRight.push(prevIdx - 1);
+                    prevSmallerElStack.push(prevIdx);
+                }
+                else{
+                    suffixRight.push(heightsLen - 1);
+                }
+            }
+            else{
+                suffixRight.push(heightsLen - 1);
+            }
+
+            prevSmallerElStack.push(idxI);
+        }
+
+        suffixRight = suffixRight.reverse();
+
+        for(let heightIdx = 0; heightIdx < heightsLen; heightIdx++){
+            const currHeight = sqHeights[heightIdx];
+
+            const leftSmallerElIdx = prefixLeft[heightIdx];
+            const rightSmallerElIdx = suffixRight[heightIdx];
+            const currWidth = Math.abs(leftSmallerElIdx - rightSmallerElIdx) + 1;
+
+            const sqAreaVal = Math.min(currHeight, currWidth);
+            const newMaxSqArea = sqAreaVal ** 2;
+
+            maxSqArea = Math.max(maxSqArea, newMaxSqArea);
+        }
+
+        return maxSqArea;
+    }
+
+    // const matricesMaxSqArea = largestSqArea([2,1,5,6,2,3]);
+    // console.log(matricesMaxSqArea);
+    let matricesMaxSqArea = 0;
+
+    const mRows = matrix.length;
+    const nCols = matrix[0].length;
+    const sqHeights = new Array(nCols).fill(0);
+
+    for(let rowIdx = 0; rowIdx < mRows; rowIdx++){
+        for(let colIdx = 0; colIdx < nCols; colIdx++){
+            const currCellVal = matrix[rowIdx][colIdx];
+
+            if(Number(currCellVal) === 0){
+                sqHeights[colIdx] = 0;
+            }
+            else{
+                sqHeights[colIdx] += 1;
+            }
+        }
+
+        const newMatricesMaxSqArea = largestSqArea(sqHeights);
+        matricesMaxSqArea = Math.max(matricesMaxSqArea, newMatricesMaxSqArea);
+    }
+
+    return matricesMaxSqArea;
+};
