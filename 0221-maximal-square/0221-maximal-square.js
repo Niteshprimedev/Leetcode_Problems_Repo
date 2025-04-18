d18f3be392f4f0422366359effd2bf39d8358053
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-18 19:00:33 +0530

    Time: 20 ms (67.46%), Space: 62.8 MB (76.6%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.js
+++ 0221-maximal-square/0221-maximal-square.js
@@ -6,52 +6,104 @@ var maximalSquare = function(matrix) {
     // SAME Logic as finding the maximum rectangle area in a histogram
     // Just need to make sure that the height and the width are same;
 
+    // Logic: Not using the leftSmallerElIdx Arr and rightSmallerElIdx Arr
+    // cause the stack is helping us to get the leftSmaller element and the
+    // rightSmaller element for the popped stack element;
+    // The element that is popped will have its left Smaller element either as the 
+    // next lastIdx element in the stack or if stack is empty then 0;
+    // The element that is popped will have its right Smaller element either as 
+    // the current smaller element that is about to push in the stack or
+    // the heightsLen - 1
+
+    // Intuition:
+    // We use a monotonic increasing stack to track indices of bars. 
+    // When we encounter a shorter bar, we pop taller bars and calculate area 
+    // using the popped height and distance to nearest smaller bars on both sides.
+
     function largestSqArea(sqHeights){
         let maxSqArea = 0;
         const heightsLen = sqHeights.length;
 
-        const prefixLeft = [];
+        const prevSmallerElStack = []; // Stores indices of bars in increasing order of height
+        for(let heightIdx = 0; heightIdx < heightsLen; heightIdx++){
+            const currHeight = sqHeights[heightIdx];
 
-        let prevSmallerElStack = []
+            let lastIdx = prevSmallerElStack.length - 1;
+            // Pop all taller or equal bars, calculate area with each
+            while(prevSmallerElStack.length > 0 && sqHeights[prevSmallerElStack[lastIdx]] >= currHeight){
 
-        for(let idxI = 0; idxI < heightsLen; idxI++){
-            const currHeight = sqHeights[idxI];
+                const prevElIdx = prevSmallerElStack.pop();
+                lastIdx = prevSmallerElStack.length - 1;
 
-            while(sqHeights[prevSmallerElStack[prevSmallerElStack.length - 1]] >= currHeight && prevSmallerElStack.length > 0){
-                prevSmallerElStack.pop();
+                const newMaxSqArea = findNewMaxSqArea(prevElIdx, heightIdx, lastIdx);
+                
+                maxSqArea = Math.max(maxSqArea, newMaxSqArea);
             }
-
-            prefixLeft[idxI] = prevSmallerElStack.length === 0 ? 0 : prevSmallerElStack[prevSmallerElStack.length - 1] + 1;
-
-            prevSmallerElStack.push(idxI);
+            // Push current bar index
+            prevSmallerElStack.push(heightIdx);
         }
 
-        // console.log(prefixLeft)
+        // Calculate Area for remaining bars
+        while(prevSmallerElStack.length > 0){
 
-        prevSmallerElStack = [];
+            const prevElIdx = prevSmallerElStack.pop();
+            let lastIdx = prevSmallerElStack.length - 1;
+            
+            // Right boundary is end of array, if heights are traversed first;
+            const newMaxSqArea = findNewMaxSqArea(prevElIdx, heightsLen, lastIdx);
+            
+            maxSqArea = Math.max(maxSqArea, newMaxSqArea);
+        }
 
-        for(let idxI = heightsLen - 1; idxI >= 0; idxI--){
-            const currHeight = sqHeights[idxI];
+        // Helper function to calculate rectangle area
+        function findNewMaxSqArea(prevElIdx, heightIdx, lastIdx){
+            let leftSmallerElIdx = 0;
+            let rightSmallerElIdx = heightsLen - 1;
 
-            const leftSmallerElIdx = prefixLeft[idxI];
-            let rightSmallerElIdx;
-
-            while(sqHeights[prevSmallerElStack[prevSmallerElStack.length - 1]] >= currHeight && prevSmallerElStack.length > 0){
-                prevSmallerElStack.pop();
-            }
-
-            rightSmallerElIdx = prevSmallerElStack.length === 0 ? heightsLen - 1 : prevSmallerElStack[prevSmallerElStack.length - 1] - 1;
-
-            const currWidth = Math.abs(leftSmallerElIdx - rightSmallerElIdx) + 1;
-
-            const sqAreaVal = Math.min(currHeight, currWidth);
+            const prevElHeight = sqHeights[prevElIdx];
+            
+            // If stack is empty, left boundary is -1
+            leftSmallerElIdx = lastIdx < 0 ? -1 : prevSmallerElStack[lastIdx];
+            rightSmallerElIdx = heightIdx;
+            
+            const currWidth = rightSmallerElIdx - leftSmallerElIdx - 1; // Distance between smaller bars
+            const sqAreaVal = Math.min(prevElHeight, currWidth);
             const newMaxSqArea = sqAreaVal ** 2;
 
-            maxSqArea = Math.max(maxSqArea, newMaxSqArea);
-            prevSmallerElStack.push(idxI);
+            // Debugger Beauty goes here:
+            // console.log(`Height: ${currHeight}, Width: ${currWidth}, Area: ${newMaxSqArea}`);
+            return newMaxSqArea;
         }
 
         return maxSqArea;
+
+        /** 
+
+        // Striver Version:
+        for(let heightIdx = 0; heightIdx <= heightsLen; heightIdx++){
+            while(prevSmallerElStack.length > 0 && (heightIdx === heightsLen || sqHeights[prevSmallerElStack.length - 1] >= sqHeights[heightIdx])){
+                const prevElHeight = sqHeights[prevSmallerElStack.length - 1];
+                prevSmallerElStack.pop();
+
+                let currWidth;
+                if(prevSmallerElStack.length === 0){
+                    currWidth = heightIdx;
+                }
+                else{
+                    currWidth = heightIdx - prevSmallerElStack[prevSmallerElStack.length - 1] - 1;
+                }
+
+                const sqAreaVal = Math.min(prevElHeight, currWidth);
+                const newMaxSqArea = sqAreaVal ** 2;
+
+                maxSqArea = Math.max(maxSqArea, newMaxSqArea);
+            }
+
+            prevSmallerElStack.push(heightIdx);
+        }
+
+        return maxSqArea;
+        */
     }
 
     // const matricesMaxSqAreaT = largestSqArea([2,1,5,6,2,3]);
