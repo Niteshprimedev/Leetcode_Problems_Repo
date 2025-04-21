d57c21ed02a2d4513907908286eb89da89d995fc
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-21 06:15:04 +0530

    Time: 33 ms (5.08%), Space: 56.9 MB (42.27%) - LeetHub

--- 0139-word-break/0139-word-break.js
+++ 0139-word-break/0139-word-break.js
@@ -10,6 +10,7 @@ var wordBreak = function(s, wordDict) {
     // it means the string can't be segmented into a space separated
     // sequence
 
+    /**
     // Top Down Approach Solution:
     const strLen = s.length;
     const wordsDictMap = new Map();
@@ -56,6 +57,56 @@ var wordBreak = function(s, wordDict) {
         return isBreakingOrContinueGivesSeq;
     }
     return allSeqsWords(0, '');
+
+    */
+
+    // Top Down Approach Solution:
+    // Without moving idx 1 to the right;
+    const strLen = s.length;
+    const wordsDictMap = new Map();
+
+    for(let word of wordDict){
+        wordsDictMap.set(word, true);
+    }
+
+    // console.log(wordsDictMap);
+
+    // const memoDP = new Array(strLen).fill(-1).map(() => new Array());
+    const memoMap = new Map();
+
+    function allSeqsWords(currIdx, currStr){
+        // Base Case:
+        if(currIdx < 0){
+            // currStr += s[0];
+            // console.log(currStr);
+            const isWordPresent = wordsDictMap.has(currStr);
+            if(isWordPresent){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        const hashKey = `${currIdx}-${currStr}`;
+        if(memoMap.has(hashKey)){
+            return memoMap.get(hashKey);
+        }
+
+        let breakWordAndNewSeq = false;
+        const isWordPresent = wordsDictMap.has(currStr);
+        if(isWordPresent){
+            breakWordAndNewSeq = allSeqsWords(currIdx - 1, s[currIdx]);
+        }
+
+        const continuousStrSeq = allSeqsWords(currIdx - 1, s[currIdx] + currStr);
+
+        const isBreakingOrContinueGivesSeq = breakWordAndNewSeq || continuousStrSeq;
+        memoMap.set(hashKey, isBreakingOrContinueGivesSeq);
+
+        return isBreakingOrContinueGivesSeq;
+    }
+    return allSeqsWords(strLen - 1, '');
     
     /**
     // Bottom Up Approach Solution:
