634dd3b6a40f949443a1e6dae765b9eea7c29a7f
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-23 18:44:36 +0530

    Time: 424 ms (21.68%), Space: 28.1 MB (24.69%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
+        '''
         # Same Logic as Buy and Sell to Make profits but this time
         # a Transaction fees is included
         # Ref: Buy & Sell Sstock Part II
@@ -38,4 +39,37 @@ class Solution:
             return memo_dp[curr_price_idx][buy_flag]
         
         return all_days_buy_sell_profits(0, 1)
+        '''
 
+        # Bottom Up Solution:
+
+        total_days = len(prices)
+
+        memo_dp = [[-1 for _ in range(2)] for _ in range(total_days + 1)]
+
+        # Base Case:
+        for buy_flag in range(1, -1, -1):
+            memo_dp[total_days][buy_flag] = 0
+        
+        for curr_price_idx in range(total_days - 1, -1, -1):
+            for buy_flag in range(1, -1, -1):
+
+                max_profit = 0
+                # Buying case:
+                if(buy_flag == 1):
+                    chose_to_buy = -prices[curr_price_idx] + memo_dp[curr_price_idx + 1][0]
+
+                    skip_to_buy = memo_dp[curr_price_idx + 1][1]
+
+                    max_profit = max(chose_to_buy, skip_to_buy)
+                # Selling Case:
+                else:
+                    chose_to_sell = (prices[curr_price_idx] - fee) + memo_dp[curr_price_idx + 1][1]
+
+                    skip_to_sell = memo_dp[curr_price_idx + 1][0]
+
+                    max_profit = max(chose_to_sell, skip_to_sell)
+
+                memo_dp[curr_price_idx][buy_flag] = max_profit
+
+        return memo_dp[0][1]
