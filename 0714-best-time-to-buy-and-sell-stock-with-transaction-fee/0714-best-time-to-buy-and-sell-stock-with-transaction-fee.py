f8ae34ecdc69cb28924ffbae26b08c4e0901a637
Nitesh Sharma <niteshprimedev@gmail.com>
2025-04-23 18:52:08 +0530

    Time: 124 ms (54.63%), Space: 23.6 MB (55.28%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,5 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
+        # PreReq: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+        
         '''
         # Same Logic as Buy and Sell to Make profits but this time
         # a Transaction fees is included
