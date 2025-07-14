540ef9fea98308aa8f0ecd219c1c59f2aca35a94
Nitesh Sharma <niteshprimedev@gmail.com>
2025-07-14 11:04:10 +0530

    Time: 0 ms (100%), Space: 17.8 MB (75.12%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.py
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -1,5 +1,6 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
+        '''
         # Solution using Max Heap and chars frequency logic;
         str_chars_freq_hash_map = collections.Counter(s)
 
@@ -33,6 +34,7 @@ class Solution:
                 reorganized_str_list.append(top_char)
         
         return "".join(reorganized_str_list)
+        '''
         
         '''
         # GFG Time Solution:
@@ -66,3 +68,39 @@ class Solution:
             
         return ''.join(rearrange_str_list)
         '''
+
+        max_heap = []
+
+        reorganized_str = ''
+
+        chars_freq_map = [0] * 26
+
+        for char in s:
+            char_idx = ord(char) - ord('a')
+            chars_freq_map[char_idx] += 1
+        
+        for idx in range(26):
+            if chars_freq_map[idx] > 0:
+                char_freq = -1 * chars_freq_map[idx]
+                char = chr(idx + 97)
+                heapq.heappush(max_heap, (char_freq, char))
+
+        while len(max_heap) >= 2:
+            first_char_freq, first_char = heapq.heappop(max_heap)
+            second_char_freq, second_char = heapq.heappop(max_heap)
+
+            reorganized_str += first_char + second_char
+
+            if (-1 * first_char_freq) > 1:
+                heapq.heappush(max_heap, (first_char_freq + 1, first_char))
+            if (-1 * second_char_freq) > 1:
+                heapq.heappush(max_heap, (second_char_freq + 1, second_char))
+        
+        if max_heap:
+            # print(max_heap[0][0], reorganized_str[-1])
+            if max_heap[0][0] != -1 or reorganized_str != '' and reorganized_str[-1] == max_heap[0][1]:
+                return ''
+            else:
+                reorganized_str += max_heap[0][1]
+        
+        return reorganized_str
