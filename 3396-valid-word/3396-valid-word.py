d619fc218e82ddec5c38b4185d50731de500546c
Nitesh Sharma <niteshprimedev@gmail.com>
2025-07-15 08:06:57 +0530

    Time: 0 ms (100%), Space: 17.7 MB (86.06%) - LeetHub

--- 3396-valid-word/3396-valid-word.py
+++ 3396-valid-word/3396-valid-word.py
@@ -22,6 +22,8 @@ class Solution:
 
         return False
         '''
+
+        '''
         # Solution to check only if it contains a vowel or consonant 
         # or not?
         # Implementation and Programming Skill
@@ -34,6 +36,28 @@ class Solution:
                 if char in ('aeiouAEIOU'):
                     is_vowel_present = True
                 elif char.isalnum() and char not in ('0123456789'):
+                    is_cons_present = True
+                elif not char.isalnum():
+                    return False
+            
+            if is_vowel_present and is_cons_present:
+                return True
+                
+        return False
+        '''
+
+        # Solution to check only if it contains a vowel or consonant 
+        # or not?
+        # Implementation and Programming Skill
+
+        # Contains a min of 3 chars
+        if len(word) >= 3:
+            is_vowel_present = False
+            is_cons_present = False
+            for char in word:
+                if char in ('aeiouAEIOU'):
+                    is_vowel_present = True
+                elif char.isalpha():
                     is_cons_present = True
                 elif not char.isalnum():
                     return False
