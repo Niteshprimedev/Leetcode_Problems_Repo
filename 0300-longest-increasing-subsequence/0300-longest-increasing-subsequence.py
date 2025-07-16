44cbb8d2eca82de0c586c92f5d3fccf7685aeb33
Nitesh Sharma <niteshprimedev@gmail.com>
2025-07-16 09:23:00 +0530

    Time: 11 ms (78.08%), Space: 17.9 MB (80.77%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,65 +1,49 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        '''
-        # Top Down: Memoization Approach gives TLE
-        n = len(nums)
-        # Edge Case:
-        if n == 1:
-            return 1
-
-        memo_dp = [[-1 for _ in range(n + 1)] for _ in range(n)]
-
-        def all_inc_subsequences(curr_idx, prev_idx):
-            # Base Case:
-            if curr_idx == (n - 1):
-                if nums[curr_idx] > nums[prev_idx]:
-                    return 1
-                else:
-                    return 0
+        # code here
+        # Binary Search Solution:
+        # Intuition:
+        # While traversing we try to create multiple 
+        # LIS forms and then later on figure out the max one
+        # But instead of tracking the multiple instances
+        # of LIS, we can overwrite the existing LIS whenever 
+        # we find any element which is smaller than the last element
+        # in our current LIS
+        # This way, we ensure the length of longest increasing subsequence
+        # Note: we overwrite the currEl with our existing element in LIS
+        # at the position where the element is found or at the next greater
+        # element position; 
+        
+        def binary_search_idx(sorted_arr, target):
+            left_idx = 0
+            right_idx = len(sorted_arr) - 1
             
-            if memo_dp[curr_idx][prev_idx + 1] != -1:
-                return memo_dp[curr_idx][prev_idx + 1]
-
-            # Pick Case:
-            pick_case = 0
-            if prev_idx == -1 or nums[curr_idx] > nums[prev_idx]:
-                pick_case = 1 + all_inc_subsequences(curr_idx + 1, curr_idx)
-
-            # Not Pick Case
-            not_pick_case = 0 + all_inc_subsequences(curr_idx + 1, prev_idx)
-
-            memo_dp[curr_idx][prev_idx + 1] = max(pick_case, not_pick_case)
-            # print(memo_dp)
-            return memo_dp[curr_idx][prev_idx + 1]
+            while(left_idx < right_idx):
+                middle_idx = left_idx + (right_idx - left_idx) // 2
+                
+                if sorted_arr[middle_idx] >= target:
+                    right_idx = middle_idx
+                else:
+                    left_idx = middle_idx + 1
+                    
+            return left_idx
         
-        return all_inc_subsequences(0, -1)
-        '''
+        longest_els_len = []
+        longest_inc_subseq_len = 1
         
-        # Bottom Up: Tabulation Approach 
-        n = len(nums)
-        # Edge Case:
-        if n == 1:
-            return 1
-
-        memo_dp = [[0 for _ in range(n + 1)] for _ in range(n)]
-
-        # Base Case:
-        for prev_idx in range(n - 2, -2, -1):
-            if prev_idx == -1 or nums[n - 1] > nums[prev_idx]:
-                memo_dp[n - 1][prev_idx + 1] = 1
+        longest_els_len.append(nums[0])
         
-        for curr_idx in range(n - 2, -1, -1):
-            for prev_idx in range(curr_idx - 1, -2, -1):
-                # Pick Case:
-                pick_case = 0
-                if prev_idx == -1 or nums[curr_idx] > nums[prev_idx]:
-                    pick_case = 1 + memo_dp[curr_idx + 1][curr_idx + 1]
-
-                # Not Pick Case
-                not_pick_case = 0 + memo_dp[curr_idx + 1][prev_idx + 1]
-
-                memo_dp[curr_idx][prev_idx + 1] = max(pick_case, not_pick_case)
-                # print(memo_dp)
+        for curr_idx in range(1, len(nums)):
+            last_idx = len(longest_els_len) - 1
+            
+            if nums[curr_idx] > longest_els_len[last_idx]:
+                longest_els_len.append(nums[curr_idx])
+                longest_inc_subseq_len += 1
+            else:
+                # Binary Search: 
+                # If the element exists then it's index
+                # else the index of the next greater element;
+                found_idx = binary_search_idx(longest_els_len, nums[curr_idx])
+                longest_els_len[found_idx] = nums[curr_idx]
         
-        # print(memo_dp)
-        return memo_dp[0][0]
+        return longest_inc_subseq_len
