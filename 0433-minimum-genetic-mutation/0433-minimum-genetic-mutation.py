9a56e27131fc66b5f6d2900a073e783e1b7b827a
Nitesh Sharma <niteshprimedev@gmail.com>
2025-07-17 10:21:37 +0530

    Time: 0 ms (100%), Space: 17.8 MB (40.68%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -44,32 +44,26 @@ class Solution:
         elif startGene != endGene and endGene not in gene_banks_map:
             return -1
 
-        def graph_bfs(curr_gene):
-            genes_queue = []
+        genes_queue = []
 
-            genes_queue.append((curr_gene, 0))
-            visited_set = set()
+        genes_queue.append((startGene, 0))
+        visited_set = set()
+        visited_set.add(startGene)
 
-            min_mutations = -1
-            while len(genes_queue):
-                curr_gene, curr_mutation = genes_queue.pop(0)
+        min_mutations = -1
+        while len(genes_queue):
+            curr_gene, curr_mutation = genes_queue.pop(0)
 
-                if curr_gene == endGene:
-                    min_mutations = curr_mutation
-                    break
+            if curr_gene == endGene:
+                min_mutations = curr_mutation
+                break
 
-                if curr_gene not in visited_set:
-                    visited_set.add(curr_gene)
-
-                    for char_idx in range(len(curr_gene)):
-                        for nucleotide in 'ACGT':
-                            if curr_gene[char_idx] != nucleotide:
-                                next_gene = curr_gene[:char_idx] + nucleotide + curr_gene[char_idx + 1:]
-                                if next_gene in gene_banks_map and next_gene not in visited_set:
-                                    genes_queue.append((next_gene, curr_mutation + 1))
+            for char_idx in range(len(curr_gene)):
+                for nucleotide in 'ACGT':
+                    if curr_gene[char_idx] != nucleotide:
+                        next_gene = curr_gene[:char_idx] + nucleotide + curr_gene[char_idx + 1:]
+                        if next_gene in gene_banks_map and next_gene not in visited_set:
+                            genes_queue.append((next_gene, curr_mutation + 1))
+                            visited_set.add(next_gene)
             
-            return min_mutations
-
-        min_mutations = graph_bfs(startGene)
-
         return min_mutations
