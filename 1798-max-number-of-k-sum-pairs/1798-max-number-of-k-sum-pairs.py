96d1fb30408bb2079a83b4f73122353a7626b880
Nitesh Sharma <niteshprimedev@gmail.com>
2025-07-18 06:42:11 +0530

    Time: 542 ms (11.39%), Space: 31.9 MB (5.31%) - LeetHub

---
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        # Logic: Sort the nums array using built-in or sorting algos
+        # and then use two pointers technique to find k-sum pairs;
+        
+        # Solution 3: Using Hashmap and No Sorting;
+        # Just need to count unique pairs equal k-sum;
+        max_pairs_count = 0
+
+        right_side_els_map = defaultdict(int)
+
+        for num in nums:
+            right_side_els_map[num] += 1
+        
+        for first_num in nums:
+            right_side_els_map[first_num] -= 1
+
+            second_num = k - first_num
+
+            if right_side_els_map[second_num] > 0 and right_side_els_map[first_num] >= 0:
+                max_pairs_count += 1
+                right_side_els_map[second_num] -= 1
+        
+        return max_pairs_count
+
+
+
+
+
