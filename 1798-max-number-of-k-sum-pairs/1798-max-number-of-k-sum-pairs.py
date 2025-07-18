6308695681790a3555ecb53fd242caadd830c06b
Nitesh Sharma <niteshprimedev@gmail.com>
2025-07-18 06:44:31 +0530

    Time: 503 ms (63.9%), Space: 30.9 MB (9.91%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
@@ -3,6 +3,7 @@ class Solution:
         # Logic: Sort the nums array using built-in or sorting algos
         # and then use two pointers technique to find k-sum pairs;
         
+        '''
         # Solution 3: Using Hashmap and No Sorting;
         # Just need to count unique pairs equal k-sum;
         max_pairs_count = 0
@@ -20,6 +21,25 @@ class Solution:
             if right_side_els_map[second_num] > 0 and right_side_els_map[first_num] >= 0:
                 max_pairs_count += 1
                 right_side_els_map[second_num] -= 1
+        
+        return max_pairs_count
+        '''
+        
+        # Solution 4: Using Hashmap and No Sorting;
+        # Just need to count unique pairs equal k-sum;
+        # Only Pass Solution
+        max_pairs_count = 0
+
+        nums_els_map = defaultdict(int)
+
+        for first_num in nums:
+            second_num = k - first_num
+
+            if nums_els_map[second_num] > 0:
+                max_pairs_count += 1
+                nums_els_map[second_num] -= 1
+            else:
+                nums_els_map[first_num] += 1
         
         return max_pairs_count
 
