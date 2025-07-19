dbfa284e6d02c3aec74651b197b721ae6effff5b
Nitesh Sharma <niteshprimedev@gmail.com>
2025-07-19 18:45:11 +0530

    Time: 9 ms (90.14%), Space: 20.8 MB (56.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -29,4 +29,52 @@ class Solution:
         return list(str_group_map.values())
         '''
 
+        # Solution 3: Using Map and Merge_Sort;
+        def merge(strt_idx, mid, end_idx, str_arr):
+            merged_arr = []
+
+            idx_i = strt_idx
+            idx_j = mid + 1
+
+            while idx_i <= mid and idx_j <= end_idx:
+                if ord(str_arr[idx_i]) <= ord(str_arr[idx_j]):
+                    merged_arr.append(str_arr[idx_i])
+                    idx_i += 1
+                else:
+                    merged_arr.append(str_arr[idx_j])
+                    idx_j += 1
+            
+            while idx_i <= mid:
+                merged_arr.append(str_arr[idx_i])
+                idx_i += 1
+
+            while idx_j <= end_idx:
+                merged_arr.append(str_arr[idx_j])
+                idx_j += 1
+
+            for idx in range(len(merged_arr)):
+                str_arr[idx + strt_idx] = merged_arr[idx]
+
+            return str_arr
+
+        def merge_sort(strt_idx, end_idx, str_arr):
+            if strt_idx < end_idx:
+                mid = strt_idx + (end_idx - strt_idx) // 2
+
+                merge_sort(strt_idx, mid, str_arr)
+                merge_sort(mid + 1, end_idx, str_arr)
+
+                merge(strt_idx, mid, end_idx, str_arr)
+
+            return str_arr
+        
+        str_group_map = defaultdict(list)
+
+        for curr_str in strs:
+            sorted_str = merge_sort(0, len(curr_str) - 1, list(curr_str))
+
+            str_group_map[tuple(sorted_str)].append(curr_str) 
+
+        return [values for hash_key, values in sorted_hashkey_group_value_map.items()]
+
         
