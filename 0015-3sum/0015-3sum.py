8869c716701e4e49d3d42c02f5e653c5063abc64
Nitesh Sharma <niteshprimedev@gmail.com>
2025-07-21 13:10:24 +0530

    Time: 640 ms (31.69%), Space: 20.7 MB (36.62%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -38,13 +38,92 @@ class Solution:
         
         return triplets_sum_equals_zero
         '''
-            
+        
+        '''
         # Solution: Without using nested while loop;
         # The nested loop is important to have skip the duplicates
         # from the start but the third_num can also be repeating so need to skip
         # the second_num before checking for triplets_sum;
         # Problem No. 1
         nums = sorted(nums)
+        triplets_sum_equals_zero = []
+
+        for first_idx in range(len(nums)):
+            first_num = nums[first_idx]
+            # Skipping all the duplicates where first_num
+            # is same as previous first_num value;
+            if(first_idx > 0 and first_num == nums[first_idx - 1]):
+                continue
+            
+            second_idx = first_idx + 1
+            third_idx = len(nums) - 1
+
+            while (second_idx < third_idx):
+                second_num = nums[second_idx]
+                third_num = nums[third_idx]
+
+                triplets_sum = first_num + second_num + third_num
+
+                if triplets_sum == 0:
+                    triplets_sum_equals_zero.append([first_num, second_num, third_num])
+                    second_idx +=1
+                    third_idx -= 1
+
+                    # Skipping all the duplicates where second_num
+                    # is same as previous second_num value; cause the
+                    # third num will also be repeating so it is important;
+                    while second_idx < third_idx and nums[second_idx] == second_num:
+                        second_idx += 1
+                elif triplets_sum > 0:
+                    third_idx -= 1
+                else:
+                    second_idx += 1
+        
+        return triplets_sum_equals_zero
+        '''
+
+        # Solution 3: Using Map and Merge_Sort;
+        def merge(strt_idx, mid, end_idx, nums):
+            merged_arr = []
+
+            idx_i = strt_idx
+            idx_j = mid + 1
+
+            while idx_i <= mid and idx_j <= end_idx:
+                if nums[idx_i] <= nums[idx_j]:
+                    merged_arr.append(nums[idx_i])
+                    idx_i += 1
+                else:
+                    merged_arr.append(nums[idx_j])
+                    idx_j += 1
+            
+            while idx_i <= mid:
+                merged_arr.append(nums[idx_i])
+                idx_i += 1
+
+            while idx_j <= end_idx:
+                merged_arr.append(nums[idx_j])
+                idx_j += 1
+
+            for idx in range(len(merged_arr)):
+                nums[idx + strt_idx] = merged_arr[idx]
+
+            return nums
+
+        def merge_sort(strt_idx, end_idx, nums):
+            if strt_idx < end_idx:
+                mid = strt_idx + (end_idx - strt_idx) // 2
+
+                merge_sort(strt_idx, mid, nums)
+                merge_sort(mid + 1, end_idx, nums)
+
+                merge(strt_idx, mid, end_idx, nums)
+
+            return nums
+        
+        n = len(nums)
+        merge_sort(0, n - 1, nums)
+
         triplets_sum_equals_zero = []
 
         for first_idx in range(len(nums)):
