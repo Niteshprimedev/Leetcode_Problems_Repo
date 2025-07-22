d2de1212472981051bcd477ef029764ffd53455d
Nitesh Sharma <niteshprimedev@gmail.com>
2025-07-22 18:47:41 +0530

    Time: 73 ms (30.44%), Space: 21.8 MB (68.34%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
@@ -28,6 +28,7 @@ class Solution:
         return longest_subarr_len
         '''
 
+        '''
         # Solution 2: Optimal Solution;
         # Logic: Expand the window as long as the condition is valid
         # and then maintain the longest window as long as the condition is invalid
@@ -56,4 +57,33 @@ class Solution:
 
             longest_subarr_len = max(longest_subarr_len, window_size)
         
+        return longest_subarr_len
+        '''
+
+        # Solution 3: Tweaking the if Condition to <=
+        # Logic: using total flips count logic
+        # and if all the elements are one then delete one 1 element
+
+        window_strt_idx = 0
+
+        max_el_del_count = 0
+
+        longest_subarr_len = 0
+
+        for window_end_idx in range(len(nums)):
+            curr_num = nums[window_end_idx]
+
+            if curr_num == 0:
+                max_el_del_count += 1
+
+            while max_el_del_count > 1 and window_strt_idx <= window_end_idx:
+                if nums[window_strt_idx] == 0:
+                    max_el_del_count -= 1
+                window_strt_idx += 1
+            
+            # deleting 1 element so no need to add 1;
+            window_size = window_end_idx - window_strt_idx
+
+            longest_subarr_len = max(longest_subarr_len, window_size)
+
         return longest_subarr_len
