10cfbd8228668f0fc289fc15476067812c135c3a
Nitesh Sharma <niteshprimedev@gmail.com>
2025-07-23 15:57:00 +0530

    Time: 71 ms (27.32%), Space: 19.7 MB (83.34%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -1,5 +1,6 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        '''
         # Logic: Using the striver's video solution;
         # How the `right - left + 1` formula works?
         # cause it gives us the number of *unique* subarrays ending at "right" index!
@@ -24,6 +25,35 @@ class Solution:
                 prefix_product *= curr_num
 
                 while window_strt_idx < (window_end_idx + 1) and prefix_product >= k:
+                    prefix_product /= nums[window_strt_idx]
+                    window_strt_idx += 1
+                
+                total_subarrs_count += window_end_idx - window_strt_idx + 1
+            
+            return total_subarrs_count
+        
+        total_k_subarrs_product = countSubArrs(nums, k)
+        return total_k_subarrs_product
+        '''
+
+        # Solution 2: Modifying the while condition;
+        if k < 2:
+            return 0
+
+        def countSubArrs(nums, k):
+            if k < 0:
+                return 0
+            
+            total_subarrs_count = 0
+            window_strt_idx = 0
+            prefix_product = 1
+            
+            for window_end_idx in range(len(nums)):
+                curr_num = nums[window_end_idx]
+
+                prefix_product *= curr_num
+
+                while window_strt_idx <= window_end_idx and prefix_product >= k:
                     prefix_product /= nums[window_strt_idx]
                     window_strt_idx += 1
                 
