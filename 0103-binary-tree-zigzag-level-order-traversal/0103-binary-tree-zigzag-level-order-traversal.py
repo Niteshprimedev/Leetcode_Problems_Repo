cc5e60d4bc5b45508beb5d151df56b34e6137261
Nitesh Sharma <niteshprimedev@gmail.com>
2025-07-25 16:20:19 +0530

    Time: 0 ms (100%), Space: 18.1 MB (48.15%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -0,0 +1,92 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        '''
+        # Logic: BFS Traversal with each alternate level nodes reversed
+
+        zig_zag_level_traversal = []
+
+        if not root:
+            return zig_zag_level_traversal
+
+        queue = []
+        queue.append(root)
+        node_idx = 0
+        curr_level = 0
+
+        while(node_idx < len(queue)):   
+            curr_level += 1
+            level_len = len(queue)
+
+            curr_level_nodes = []
+
+            for idx_i in range(node_idx, level_len):
+                current_node = queue[node_idx]
+
+                node_idx += 1
+
+                if current_node.left:
+                    queue.append(current_node.left)
+                if current_node.right:
+                    queue.append(current_node.right)
+
+                curr_level_nodes.append(current_node.val)
+            
+            if curr_level % 2 == 0:
+                curr_level_nodes = curr_level_nodes[::-1]
+
+            zig_zag_level_traversal.append(curr_level_nodes)
+        
+        return zig_zag_level_traversal
+        '''
+
+        # Logic: BFS Traversal with each alternate level nodes reversed
+        # Solution 2: Strategic Traversal from left & right when 
+        # we are at odd and even levels in our traversal;
+
+        zig_zag_level_traversal = []
+
+        if not root:
+            return zig_zag_level_traversal
+
+        queue = []
+        queue.append(root)
+        curr_level = 0
+
+        while len(queue) > 0:
+            curr_level += 1
+            level_len = len(queue)
+
+            curr_level_nodes = []
+            curr_level_stack = []
+
+            for idx_i in range(level_len):
+                current_node = queue.pop(0)
+
+                if curr_level % 2 == 0:
+                    # on even levels push the right & left
+                    if current_node.right:
+                        curr_level_stack.append(current_node.right)
+                    if current_node.left:
+                        curr_level_stack.append(current_node.left)
+                else:
+                    # on odd levels push the left & right
+                    if current_node.left:
+                        curr_level_stack.append(current_node.left)
+                    if current_node.right:
+                        curr_level_stack.append(current_node.right)
+
+                curr_level_nodes.append(current_node.val)
+
+            zig_zag_level_traversal.append(curr_level_nodes)
+
+            # for the next level fill the nodes into the queue
+            while curr_level_stack:
+                queue.append(curr_level_stack.pop())
+        
+        return zig_zag_level_traversal
