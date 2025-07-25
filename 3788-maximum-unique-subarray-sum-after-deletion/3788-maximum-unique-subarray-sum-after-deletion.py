34a7e8c72c4803023f90f076e9dd0b56dfe8b6a8
Nitesh Sharma <niteshprimedev@gmail.com>
2025-07-25 07:58:41 +0530

    Time: 2 ms (33.96%), Space: 17.9 MB (11.24%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.py
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.py
@@ -1,5 +1,7 @@
 class Solution:
     def maxSum(self, nums: List[int]) -> int:
+        '''
+        # Solution 1: Using Kadane's Algo
         unique_els = set(nums)
 
         curr_sub_arr_sum = 0
@@ -14,3 +16,27 @@ class Solution:
             max_sub_arr_sum = max(max_sub_arr_sum, curr_sub_arr_sum)
         
         return max_sub_arr_sum
+        '''
+
+        # Solution 2: Most Optimized Hint Solution:
+        unique_els = set()
+        max_sub_arr_sum = -101
+
+        for num in nums:
+            max_sub_arr_sum = max(max_sub_arr_sum, num)
+            unique_els.add(num)
+
+        # if the max element in arr is less than or equal than zero
+        # then the answer is the max element
+        if max_sub_arr_sum <= 0:
+            return max_sub_arr_sum
+
+        max_sub_arr_sum = 0
+        # Otherwise, the max sum is sum of all unique
+        # vals greater than or equal to zero
+        for num in unique_els:
+            if num > 0:
+                max_sub_arr_sum += num
+        
+        return max_sub_arr_sum
+
