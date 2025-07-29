bef87789616ceae587bb46a363ab10cda10ed1f2
Nitesh Sharma <niteshprimedev@gmail.com>
2025-07-29 20:36:22 +0530

    Time: 3 ms (76.17%), Space: 19.1 MB (72.4%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,6 +1,5 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        '''
         # Solution using Topo-Sort Algo & inDegree
         # Return the topo_sort_list in reverse order;
         # Kahn's BFS Topo-Sort Algorithm
@@ -47,13 +46,13 @@ class Solution:
             return topo_sort_list
         else:
             return []
-        '''
         
+        '''
         # Solution using DFS Topo-Sort Algo 
         # Return the topo_sort_list in reverse order;
 
+        in_degrees = [0] * numCourses
         topo_sort_list = []
-        visited = [False] * numCourses
 
         adj_list = {}
 
@@ -64,32 +63,34 @@ class Solution:
             if vertex1 not in adj_list:
                 adj_list[vertex1] = []
             
+            in_degrees[vertex2] += 1
             adj_list[vertex1].append(vertex2)
         
         # print(adj_list)
+
+        stack = []
+
+        for idx_i in range(len(in_degrees)):
+            degree = in_degrees[idx_i]
+
+            if degree == 0:
+                queue.append(idx_i)
         
-        def graphDFSIve(curr_vertex):
-            stack = []
+        while len(queue) > 0:
+            curr_vertex = queue.pop(0)
+            topo_sort_list.append(curr_vertex)
 
-            stack.append(curr_vertex)
+            if curr_vertex in adj_list:
+                for neighbor in adj_list[curr_vertex]:
+                    in_degrees[neighbor] -= 1
 
-            while len(stack) > 0:
-                curr_vertex = stack.pop()
-                topo_sort_list.append(curr_vertex)
-
-                if curr_vertex in adj_list:
-                    for neighbor in adj_list[curr_vertex]:
-                        if not visited[neighbor]:
-                            visited[neighbor] = True
-                            stack.append(neighbor)
-        
-        for vertex in range(numCourses):
-            if not visited[vertex]:
-                visited[vertex] = True
-                graphDFSIve(vertex)
+                    if in_degrees[neighbor] == 0:
+                        queue.append(neighbor)
 
         # print(topo_sort_list)
         if len(topo_sort_list) == numCourses:
+            topo_sort_list.reverse()
             return topo_sort_list
         else:
             return []
+        '''
