246d7b402933a6bfd84b25a61374dbce55bf1fe1
Nitesh Sharma <niteshprimedev@gmail.com>
2025-07-29 20:36:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,95 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        '''
+        # Solution using Topo-Sort Algo & inDegree
+        # Return the topo_sort_list in reverse order;
+        # Kahn's BFS Topo-Sort Algorithm
+
+        topo_sort_list = []
+        in_degrees = [0] * numCourses
+
+        adj_list = {}
+
+        for edge in prerequisites:
+            vertex1 = edge[0]
+            vertex2 = edge[1]
+
+            if vertex1 not in adj_list:
+                adj_list[vertex1] = []
+            
+            in_degrees[vertex2] += 1
+            adj_list[vertex1].append(vertex2)
+        
+        # print(adj_list)
+
+        queue = []
+
+        for idx_i in range(len(in_degrees)):
+            degree = in_degrees[idx_i]
+
+            if degree == 0:
+                queue.append(idx_i)
+        
+        while len(queue) > 0:
+            curr_vertex = queue.pop(0)
+            topo_sort_list.append(curr_vertex)
+
+            if curr_vertex in adj_list:
+                for neighbor in adj_list[curr_vertex]:
+                    in_degrees[neighbor] -= 1
+
+                    if in_degrees[neighbor] == 0:
+                        queue.append(neighbor)
+
+        # print(topo_sort_list)
+        if len(topo_sort_list) == numCourses:
+            topo_sort_list.reverse()
+            return topo_sort_list
+        else:
+            return []
+        '''
+        
+        # Solution using DFS Topo-Sort Algo 
+        # Return the topo_sort_list in reverse order;
+
+        topo_sort_list = []
+        visited = [False] * numCourses
+
+        adj_list = {}
+
+        for edge in prerequisites:
+            vertex1 = edge[0]
+            vertex2 = edge[1]
+
+            if vertex1 not in adj_list:
+                adj_list[vertex1] = []
+            
+            adj_list[vertex1].append(vertex2)
+        
+        # print(adj_list)
+        
+        def graphDFSIve(curr_vertex):
+            stack = []
+
+            stack.append(curr_vertex)
+
+            while len(stack) > 0:
+                curr_vertex = stack.pop()
+                topo_sort_list.append(curr_vertex)
+
+                if curr_vertex in adj_list:
+                    for neighbor in adj_list[curr_vertex]:
+                        if not visited[neighbor]:
+                            visited[neighbor] = True
+                            stack.append(neighbor)
+        
+        for vertex in range(numCourses):
+            if not visited[vertex]:
+                visited[vertex] = True
+                graphDFSIve(vertex)
+
+        # print(topo_sort_list)
+        if len(topo_sort_list) == numCourses:
+            return topo_sort_list
+        else:
+            return []
