d8b8efcb604319cebe44a1bcfb80a093adea2310
Nitesh Sharma <niteshprimedev@gmail.com>
2025-07-30 04:33:03 +0530

    Time: 6 ms (49.09%), Space: 58.6 MB (5.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,22 +4,69 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const numsElsHashmap = new Map();
-    let twoSumIndices = [];
+    // Logic: 
+    // Sum of two elements = target
+    // x + y = target
+    // or x = target - y;
+    // so while traversing the nums array, if we fix the 
+    // value of y at index i and then we can check whether
+    // the target - y i.e x exists in map or not
 
-    for(let firstNumIdx = 0; firstNumIdx < nums.length; firstNumIdx++){
-        const firstNumKey = nums[firstNumIdx];
-        const hashValue = firstNumIdx;
-        const secondNumHashkey = target - firstNumKey;
+    /** 
+    // But to get the same indices, we would need to
+    // maintain the same order of the array, we can't change;
+    // Sorting & Two Pointers Technique: Assuming that 
+    // we just need to find one pair;
+    nums.sort((a, b) => a - b);
 
-        const isNumAlreadyPresent = numsElsHashmap.has(secondNumHashkey);
-        if(isNumAlreadyPresent){
-            const secondNumIdx = numsElsHashmap.get(secondNumHashkey);
-            twoSumIndices.push(secondNumIdx);
-            twoSumIndices.push(firstNumIdx);
+    let firstNumIdx = 0;
+    let secondNumIdx = nums.length - 1;
+    const twoSumIndices = new Array(2);
+
+    while(firstNumIdx < secondNumIdx){
+        const firstNum = nums[firstNumIdx];
+        const secondNum = nums[secondNumIdx];
+
+        const twoNumsSum = firstNum + secondNum;
+        if(twoNumsSum === target){
+            twoSumIndices[0] = firstNumIdx;
+            twoSumIndices[1] = secondNumIdx;
+            break;
+        }
+        else if(twoNumsSum < target){
+            firstNumIdx++;
         }
         else{
-            numsElsHashmap.set(firstNumKey, hashValue);
+            secondNumIdx--;
+        }
+    }
+
+    return twoSumIndices;
+
+    */
+
+    const numsElsHashmap = new Map();
+    const twoSumIndices = [];
+
+    for(let idxI = 0; idxI < nums.length; idxI++){
+        const hashKey = nums[idxI];
+        const hashValue = idxI;
+
+        numsElsHashmap.set(hashKey, hashValue);
+    }
+
+    // console.log(numsElsHashmap);
+
+    for(let idxJ = 0; idxJ < nums.length; idxJ++){
+        const firstNum = nums[idxJ];
+
+        const secondNumKey = target - firstNum;
+
+        const secondNumIdx = numsElsHashmap.get(secondNumKey) || -1;
+        if(secondNumIdx > -1 && idxJ !== secondNumIdx){
+            twoSumIndices.push(idxJ);
+            twoSumIndices.push(secondNumIdx);
+            break;
         }
     }
 
