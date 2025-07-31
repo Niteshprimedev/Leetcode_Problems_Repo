29c10806dd8bc767133d0340885ce7c5e98dfe74
Nitesh Sharma <niteshprimedev@gmail.com>
2025-07-31 08:27:31 +0530

    Time: 77 ms (9.17%), Space: 53.6 MB (98.26%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let numberObj = {}
+    for (let val of nums){
+        numberObj[val] = (numberObj[val] || 0) + 1;
+    }
+
+    for (let numKey in numberObj) {
+        if(numberObj[numKey] === 1) {
+            return numKey;
+        }
+    }
+};
