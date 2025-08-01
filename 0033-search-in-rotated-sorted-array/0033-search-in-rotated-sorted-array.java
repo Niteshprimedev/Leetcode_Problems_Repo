36e2bd11116fed976b306d0812586af25fa35dc5
Nitesh Sharma <niteshprimedev@gmail.com>
2025-08-01 19:41:23 +0530

    Time: 0 ms (100%), Space: 42.3 MB (39.71%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,34 +1,73 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int n = nums.length;
 
-        int leftIdx = 0;
-        int rightIdx = n - 1;
+    private static int findPivot(int[] nums, int left, int right){
+        int pivotIdx = -1;
+        int target = nums[right];
 
-        while(leftIdx <= rightIdx){
-            int midIdx = leftIdx + (rightIdx - leftIdx) / 2;
+        while(left <= right){
+            int midIdx = left + (right - left) / 2;
 
+            if(nums[midIdx] > target){
+                pivotIdx = midIdx;
+                left = midIdx + 1;
+            }
+            else if(nums[midIdx] < target){
+                right = midIdx - 1;
+            }
+            else{
+                break;
+            }
+        }
+
+        return pivotIdx;
+    }
+
+    private static int findTarget(int[] nums, int left, int right, int target){
+
+        while(left <= right){
+            int midIdx = left + (right - left) / 2;
+
+            // System.out.println(midIdx + "" + nums[midIdx] + "" + target);
             if(nums[midIdx] == target){
                 return midIdx;
             }
-            else if(nums[leftIdx] <= nums[midIdx]){
-                if(nums[leftIdx] <= target && target <= nums[midIdx]){
-                    rightIdx = midIdx - 1;
-                }
-                else{
-                    leftIdx = midIdx + 1;
-                }
+            else if(nums[midIdx] > target){
+                right = midIdx - 1;
             }
             else{
-                if(nums[midIdx] <= target && target <= nums[rightIdx]){
-                    leftIdx = midIdx + 1;
-                }
-                else{
-                    rightIdx = midIdx - 1;
-                }
+                left = midIdx + 1;
             }
         }
 
         return -1;
+    }
+
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+
+        int targetIdx = -1;
+
+        int leftIdx = 0;
+        int rightIdx = n - 1;
+
+        int pivotIdx = findPivot(nums, leftIdx, rightIdx);
+
+        // System.out.println(pivotIdx);
+
+        if(pivotIdx == rightIdx){
+            targetIdx = findTarget(nums, leftIdx, rightIdx, target);
+        }
+        else{
+            targetIdx = findTarget(nums, leftIdx, pivotIdx, target);
+            // System.out.println(targetIdx);
+
+            if(targetIdx == -1){
+                targetIdx = findTarget(nums, pivotIdx + 1, rightIdx, target);
+            }
+        }
+
+        // System.out.println(targetIdx);
+        return targetIdx;
+
     }
 }
