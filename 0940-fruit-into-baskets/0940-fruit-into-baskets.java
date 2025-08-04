01ec3e94105dc4986a4b6983c1540b226117233d
Nitesh Sharma <niteshprimedev@gmail.com>
2025-08-04 12:58:22 +0530

    Time: 45 ms (85.4%), Space: 55 MB (88.25%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.java
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -1,5 +1,7 @@
 class Solution {
     public int totalFruit(int[] fruits) {
+        /*
+        // Solution 1 - Using Without Nested while Loop
         int maxFruitsCount = 0;
         int strtIdx = 0;
         int endIdx = 0;
@@ -12,6 +14,39 @@ class Solution {
             fruitsTyepeFreq.put(currFruit, hashValue);
 
             if(fruitsTyepeFreq.size() > 2){
+                int strtFruit = fruits[strtIdx];
+                hashValue = fruitsTyepeFreq.get(strtFruit);
+
+                if(hashValue == 1){
+                    fruitsTyepeFreq.remove(strtFruit);
+                }
+                else{
+                    fruitsTyepeFreq.put(strtFruit, hashValue - 1);
+                }
+                
+                strtIdx += 1;
+            }
+            
+            maxFruitsCount = Math.max(maxFruitsCount, (endIdx - strtIdx + 1));
+            endIdx += 1;
+        }
+
+        return maxFruitsCount;
+        */
+
+        // Solution 2 - Using while Nested Loop
+        int maxFruitsCount = 0;
+        int strtIdx = 0;
+        int endIdx = 0;
+        HashMap<Integer, Integer> fruitsTyepeFreq= new HashMap<>();
+
+        while(endIdx < fruits.length){
+            int currFruit = fruits[endIdx];
+            int hashValue = fruitsTyepeFreq.get(currFruit) == null ? 1 : fruitsTyepeFreq.get(currFruit) + 1;
+
+            fruitsTyepeFreq.put(currFruit, hashValue);
+
+            while(fruitsTyepeFreq.size() > 2 && strtIdx <= endIdx){
                 int strtFruit = fruits[strtIdx];
                 hashValue = fruitsTyepeFreq.get(strtFruit);
 
