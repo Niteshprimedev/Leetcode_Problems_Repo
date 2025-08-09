6b383c76751818626d3706b5e5bee355134fdbbb
Nitesh Sharma <niteshprimedev@gmail.com>
2025-08-09 21:26:22 +0530

    Time: 0 ms (100%), Space: 40.7 MB (33.91%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.java
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.java
@@ -1,6 +1,15 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
+        /*
         if(n % 2 == 0){
+            return n;
+        }
+
+        return n * 2;
+        */
+
+        // Sol2: Bit Manipulation Solution;
+        if((n & 1) == 0){
             return n;
         }
 
