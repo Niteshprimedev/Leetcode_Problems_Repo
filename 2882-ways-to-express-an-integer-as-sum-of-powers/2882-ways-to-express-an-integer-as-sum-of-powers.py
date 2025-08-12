a1ea938321c8b24e37a8d147e85a70d8ab262b2e
Nitesh Sharma <niteshprimedev@gmail.com>
2025-08-12 13:24:54 +0530

    Time: 6344 ms (5.23%), Space: 18.5 MB (58.72%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.py
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.py
@@ -1,5 +1,6 @@
 class Solution:
     def numberOfWays(self, n: int, x: int) -> int:
+        '''
         MOD = 10**9 + 7
         total_ways = 0
 
@@ -30,3 +31,35 @@ class Solution:
             return memo_dp[curr_num][curr_sum]
 
         return allPowers(1, n)
+        ''' 
+
+        # Bottom Up Approach;
+        MOD = 10**9 + 7
+        total_ways = 0
+
+        memo_dp = [[-1 for _ in range(n + 1)] for _ in range(n + 2)]
+
+        # Base Case:
+        for curr_num in range(1, n + 2):
+            for curr_sum in range(0, n + 1):
+                if curr_sum == 0:
+                    memo_dp[curr_num][curr_sum] = 1
+                else:
+                    memo_dp[curr_num][curr_sum] = 0
+        
+        for curr_num in range(n, 0, -1):
+            for curr_sum in range(1, n + 1):
+                # pick case:
+                pick_case = 0
+                not_pick_case = 0
+
+                curr_pow = curr_num ** x
+
+                if(curr_pow <= curr_sum):
+                    pick_case = memo_dp[curr_num + 1][curr_sum - curr_pow]
+                
+                not_pick_case = memo_dp[curr_num + 1][curr_sum]
+
+                memo_dp[curr_num][curr_sum] = (pick_case + not_pick_case) % MOD
+
+        return memo_dp[1][n]
