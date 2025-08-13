d2e10ab44f6e77b4008e17c6dda1eb0075f8be71
Nitesh Sharma <niteshprimedev@gmail.com>
2025-08-13 09:30:32 +0530

    Time: 47 ms (97.33%), Space: 22.2 MB (11.1%) - LeetHub

--- 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.py
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.py
@@ -6,7 +6,7 @@
 #         self.right = None
 
 class Codec:
-
+    '''
     def serialize(self, root: Optional[TreeNode]) -> str:
         """Encodes a tree to a single string.
         """
@@ -53,6 +53,58 @@ class Codec:
             
             idx[0] += 1
             if tree[idx[0]] != '*':
+                curr_node.right = TreeNode(int(tree[idx[0]]))
+                traverse(curr_node.right, idx)
+
+        traverse(deserialized_root, [0])
+
+        return deserialized_root
+    '''
+
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        """Encodes a tree to a single string.
+        """
+        if not root:
+            return ''
+
+        serialized = []
+        def traverse(curr_node):
+            if not curr_node:
+                serialized.append('null')
+                return
+            
+            serialized.append(str(curr_node.val))
+
+            traverse(curr_node.left)
+            traverse(curr_node.right)
+        
+        traverse(root)
+
+        return ' '.join(serialized)
+
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        """Decodes your encoded data to tree.
+        """
+        deserialized_root = None
+        if not data:
+            return deserialized_root
+
+        tree = data.split(' ')
+
+        deserialized_root = TreeNode(int(tree[0]))
+
+        def traverse(curr_node, idx):
+            idx[0] += 1
+
+            if(idx[0] >= len(tree)):
+                return
+    
+            if tree[idx[0]] != 'null':
+                curr_node.left = TreeNode(int(tree[idx[0]]))
+                traverse(curr_node.left, idx)
+            
+            idx[0] += 1
+            if tree[idx[0]] != 'null':
                 curr_node.right = TreeNode(int(tree[idx[0]]))
                 traverse(curr_node.right, idx)
 
