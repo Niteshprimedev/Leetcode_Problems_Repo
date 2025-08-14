3bedbc51cfde2656efbb968405766a3d5139870b
Nitesh Sharma <niteshprimedev@gmail.com>
2025-08-14 08:48:28 +0530

    Time: 2 ms (53.01%), Space: 44.8 MB (15.93%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -23,6 +23,7 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
+        /*
         if(root == null){
             return null;
         }
@@ -49,6 +50,43 @@ class Solution {
                     queue.addLast(currNode.left);
                 }
                 if(currNode.right != null){
+                    rightNode.next = currNode.right;
+                    rightNode = rightNode.next;
+                    queue.addLast(currNode.right);
+                }
+            }
+        }
+
+        return root;
+        
+         */
+
+        // Solution 2: Balanced Tree;
+        if(root == null){
+            return null;
+        }
+
+        Deque<Node> queue = new ArrayDeque<>();
+
+        queue.addLast(root);
+
+        while(queue.size() > 0){
+            Node rightNode = null;
+            int levelSize = queue.size();
+
+            for(int idx = 0; idx < levelSize; idx++){
+                Node currNode = queue.removeFirst();
+
+                if(currNode.right != null){
+                    if(rightNode == null){
+                        rightNode = currNode.left;
+                    }
+                    else{
+                        rightNode.next = currNode.left;
+                        rightNode = rightNode.next;
+                    }
+                    queue.addLast(currNode.left);
+
                     rightNode.next = currNode.right;
                     rightNode = rightNode.next;
                     queue.addLast(currNode.right);
