1cfb46b6d2807c3370a40ba8896cb836959ec6e0
Nitesh Sharma <niteshprimedev@gmail.com>
2025-08-16 23:27:23 +0530

    Time: 1 ms (17.92%), Space: 17.9 MB (31.86%) - LeetHub

--- 3945-minimum-sensors-to-cover-grid/3945-minimum-sensors-to-cover-grid.py
+++ 3945-minimum-sensors-to-cover-grid/3945-minimum-sensors-to-cover-grid.py
@@ -1,6 +1,16 @@
 import math
 class Solution:
     def minSensors(self, n: int, m: int, k: int) -> int:
+        # Intuition: To cover the max number of cells using a censor
+        # is to place it in the middle and it can cover k distance
+        # to cover all the side cells
+        # and if you try to place a censor in the middle and go 
+        # and count all the cells around it at k distance then 
+        # it will form a square of size (2*k + 1) hence you
+        # need the (2*k + 1) size of squares to cover the 
+        # entire grid with the censors and to cover the rows
+        # you need ceil number of squares for the rows and same for cols;
+        # And just multiply the rows * cols to get the ans;
         square_size = 2*k + 1
 
         # ceil number of squares for the rows;
