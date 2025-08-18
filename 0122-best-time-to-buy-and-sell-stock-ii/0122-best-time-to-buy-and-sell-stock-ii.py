5663fb76a57b11ce602265abfd8af6d7a6008f0e
Nitesh Sharma <niteshprimedev@gmail.com>
2025-08-18 09:58:30 +0530

    Time: 15 ms (7.86%), Space: 18.9 MB (30.01%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,34 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         '''
-        # Top Down Working Solution with Below Results;
-        # TLE Without MemoDP
-        # MLE with MemoDP:
-
-        n = len(prices)
-        memo_dp = [[-1 for _ in range(n + 1)] for _ in range(n)]
-
-        def all_days_buy_sell_profit(buy_idx, sell_idx):
-            # Base Case:
-            if buy_idx == n - 1 or sell_idx == n:
-                return 0
-
-            if memo_dp[buy_idx][sell_idx] != -1:
-                return memo_dp[buy_idx][sell_idx]
-
-            curr_price_sell_profit = 0
-            if prices[buy_idx] < prices[sell_idx]:
-                curr_price_sell_profit = prices[sell_idx] - prices[buy_idx] + all_days_buy_sell_profit(sell_idx + 1, sell_idx + 1)
-            
-            next_sell_price_profit = all_days_buy_sell_profit(buy_idx, sell_idx + 1)
-            next_buy_price_profit = all_days_buy_sell_profit(buy_idx + 1, buy_idx + 1)
-
-            memo_dp[buy_idx][sell_idx] = max(curr_price_sell_profit, next_sell_price_profit, next_buy_price_profit)
-            return memo_dp[buy_idx][sell_idx]
-
-        return all_days_buy_sell_profit(0, 0)
-        '''
-
         # Logic:
         # Buying and then Selling
         # Top Down Approach Solution:
@@ -76,3 +48,205 @@ class Solution:
             return memo_dp[curr_price_idx][buy_flag]
 
         return all_days_buy_sell_profit(0, 1)
+        '''
+
+        '''
+        # Bottom Up Approach Solution:
+        total_days = len(prices)
+
+        memo_dp = [[-1 for _ in range(2)] for _ in range(total_days + 1)]
+
+        # Base Case:
+        for buy_flag in range(2):
+            memo_dp[total_days][buy_flag] = 0
+        
+        # for loops:
+        for curr_price_idx in range(total_days - 1, -1, -1):
+            for buy_flag in range(1, -1, -1):
+                max_profit = 0
+
+                # Buy Case:
+                if(buy_flag == 1):
+                    # If you wanna buy then pay the current price;
+                    # And then you can sell;
+                    chose_to_buy = -prices[curr_price_idx] + memo_dp[curr_price_idx + 1][0]
+                    
+                    # If you don't wanna buy then no need to pay the current price;
+                    # And you are still searching for a better buying price so you can sell;
+                    # and maximize your profits
+                    skip_to_buy = memo_dp[curr_price_idx + 1][1]
+
+                    # max profit: for this buying & not buying decisions:
+                    max_profit = max(chose_to_buy, skip_to_buy)
+                else:
+                    # If you wanna sell then get the current price as profit;
+                    # And now you can again buy;
+                    chose_to_sell = prices[curr_price_idx] + memo_dp[curr_price_idx + 1][1]
+                    
+                    # If you don't wanna sell then no price profit at current price;
+                    # And you are still searching for a better selling price so you can sell;
+                    # and maximize your profits
+                    skip_to_sell = memo_dp[curr_price_idx + 1][0]
+
+                    max_profit = max(chose_to_sell, skip_to_sell)
+
+                memo_dp[curr_price_idx][buy_flag] = max_profit
+            
+        return memo_dp[0][1]
+        '''
+
+        # Space Optimized Bottom Up Approach Solution::
+        total_days = len(prices)
+
+        prev_dp = [-1 for _ in range(2)]
+
+        # Base Case:
+        for buy_flag in range(2):
+            prev_dp[buy_flag] = 0
+        
+        # for loops:
+        for curr_price_idx in range(total_days - 1, -1, -1):
+            curr_dp = [-1 for _ in range(2)]
+            for buy_flag in range(1, -1, -1):
+                max_profit = 0
+
+                # Buy Case:
+                if(buy_flag == 1):
+                    # If you wanna buy then pay the current price;
+                    # And then you can sell;
+                    chose_to_buy = -prices[curr_price_idx] + prev_dp[0]
+                    
+                    # If you don't wanna buy then no need to pay the current price;
+                    # And you are still searching for a better buying price so you can sell;
+                    # and maximize your profits
+                    skip_to_buy = prev_dp[1]
+
+                    # max profit: for this buying & not buying decisions:
+                    max_profit = max(chose_to_buy, skip_to_buy)
+                else:
+                    # If you wanna sell then get the current price as profit;
+                    # And now you can again buy;
+                    chose_to_sell = prices[curr_price_idx] + prev_dp[1]
+                    
+                    # If you don't wanna sell then no price profit at current price;
+                    # And you are still searching for a better selling price so you can sell;
+                    # and maximize your profits
+                    skip_to_sell = prev_dp[0]
+
+                    max_profit = max(chose_to_sell, skip_to_sell)
+
+                curr_dp[buy_flag] = max_profit
+            
+            prev_dp = curr_dp
+            
+        return prev_dp[1]
+
+        '''
+        # Space Optimized Variables Optional Bottom Up Approach Solution:
+        total_days = len(prices)
+
+        # Base Case:
+        ahead_not_buy = 0
+        ahead_buy = 0
+        
+        # for loops:
+        for curr_price_idx in range(total_days - 1, -1, -1):
+
+            # If you wanna buy then pay the current price;
+            # And then you can sell;
+            chose_to_buy = -prices[curr_price_idx] + ahead_not_buy
+            
+            # If you don't wanna buy then no need to pay the current price;
+            # And you are still searching for a better buying price so you can sell;
+            # and maximize your profits
+            skip_to_buy = ahead_buy
+
+            # max profit: for this buying & not buying decisions:
+            # Buy Case:
+            curr_buy = max(chose_to_buy, skip_to_buy)
+            
+            # If you wanna sell then get the current price as profit;
+            # And now you can again buy;
+            chose_to_sell = prices[curr_price_idx] + ahead_buy
+            
+            # If you don't wanna sell then no price profit at current price;
+            # And you are still searching for a better selling price so you can sell;
+            # and maximize your profits
+            skip_to_sell = ahead_not_buy
+
+            curr_not_buy = max(chose_to_sell, skip_to_sell)
+
+            ahead_buy = curr_buy
+            ahead_not_buy = curr_not_buy
+            
+        return ahead_buy
+        '''
+
+        '''
+        # Space Optimized Variables Optional Bottom Up Approach Solution:
+        # with buy_flag and above is without buy_flag;
+        total_days = len(prices)
+
+        # Base Case:
+        ahead_not_buy = 0
+        ahead_buy = 0
+        
+        # for loops:
+        for curr_price_idx in range(total_days - 1, -1, -1):
+            for buy_flag in range(2):
+                # Buy Case:
+                if(buy_flag == 1):
+                    # If you wanna buy then pay the current price;
+                    # And then you can sell;
+                    chose_to_buy = -prices[curr_price_idx] + ahead_not_buy
+                    
+                    # If you don't wanna buy then no need to pay the current price;
+                    # And you are still searching for a better buying price so you can sell;
+                    # and maximize your profits
+                    skip_to_buy = ahead_buy
+
+                    # max profit: for this buying & not buying decisions:
+                    ahead_buy = max(chose_to_buy, skip_to_buy)
+                else:
+                    # If you wanna sell then get the current price as profit;
+                    # And now you can again buy;
+                    chose_to_sell = prices[curr_price_idx] + ahead_buy
+                    
+                    # If you don't wanna sell then no price profit at current price;
+                    # And you are still searching for a better selling price so you can sell;
+                    # and maximize your profits
+                    skip_to_sell = ahead_not_buy
+
+                    ahead_not_buy = max(chose_to_sell, skip_to_sell)
+        
+        return ahead_buy
+        '''
+
+        '''
+        # Peek and Valley Solution:
+
+        max_profit = 0
+        price_idx = 0
+
+        while price_idx < len(prices):
+            buy_price = prices[price_idx]
+            price_idx += 1
+
+            while price_idx < len(prices) and buy_price >= prices[price_idx]:
+                buy_price = prices[price_idx]
+                price_idx += 1
+            
+            if price_idx == len(prices):
+                break
+                
+            sell_price = prices[price_idx]
+            price_idx += 1
+
+            while price_idx < len(prices) and sell_price <= prices[price_idx]:
+                sell_price = prices[price_idx]
+                price_idx += 1
+            
+            max_profit += sell_price - buy_price
+        
+        return max_profit
+        '''
