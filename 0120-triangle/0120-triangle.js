76855d8af5544d9210f8e69103a8fba2d0472a06
Nitesh Sharma <niteshprimedev@gmail.com>
2025-09-25 09:36:03 +0530

    Time: 5 ms (31.1%), Space: 55.4 MB (47.38%) - LeetHub

--- 0120-triangle/0120-triangle.js
+++ 0120-triangle/0120-triangle.js
@@ -3,6 +3,7 @@
  * @return {number}
  */
 var minimumTotal = function(triangle) {
+    /** 
     // TOP Down Approach:
     let rowsLen = triangle.length;
     const memoDP = new Array(rowsLen).fill(0).map((currEl, idx) => new Array(idx + 1).fill(0));
@@ -32,8 +33,8 @@ var minimumTotal = function(triangle) {
     // console.log(memoDP);
 
     return memoDP[0][0];
+    */
     
-    /** 
     // Bottom Up Approach;
     // If we are goingg from 0 to n in Top Down, then
     // we need to go from n to 0 in Bottom up!
@@ -63,42 +64,4 @@ var minimumTotal = function(triangle) {
     // console.log(memoDP);
 
     return memoDP[0][0];
-    */
-
-    /**
-    // Space Optimized Bottom Up Approach;
-    // If we are goingg from 0 to n in Top Down, then
-    // we need to go from n to 0 in Bottom up!
-
-    let rowsLen = triangle.length;
-    let prevDP = new Array(rowsLen).fill(0);
-    
-    // console.log(prevDP);
-    // Base Case:
-    // prevDP[0] = triangle[0][0];
-    for(let colIdx = 0; colIdx < rowsLen; colIdx++){
-        prevDP[colIdx] = triangle[rowsLen - 1][colIdx];
-    }
-
-    for(let rowIdx = rowsLen - 2; rowIdx >= 0; rowIdx--){
-        const currDP = new Array(rowIdx + 1).fill(0);
-        console.log(currDP.length, rowIdx, currDP);
-
-        for(colIdx = rowIdx; colIdx >= 0; colIdx--){
-            // Choose the currentIdx 
-            const choseCurrIdx = triangle[rowIdx][colIdx] + prevDP[colIdx];
-
-            // Choose the nextIdx
-            const choseNextIdx = triangle[rowIdx][colIdx] + prevDP[colIdx + 1];
-
-            currDP[colIdx] = Math.min(choseCurrIdx, choseNextIdx);
-        }
-
-        prevDP = currDP;
-    }   
-
-    // console.log(memoDP);
-
-    return prevDP[0];
-    */
 };
