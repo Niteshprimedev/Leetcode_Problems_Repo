f3a42839e75df9339c8a48478c460453d833b9ba
Nitesh Sharma <niteshprimedev@gmail.com>
2025-10-11 19:49:23 +0530

    Time: 25 ms (14.08%), Space: 44.8 MB (71.62%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -37,14 +37,58 @@ class Solution {
     }
 
     public boolean isMatch(String s, String p) {
+        /*
         int sLen = s.length();
 		int pLen = p.length();
 		
 		Boolean[][] memoDP = new Boolean[sLen + 1][pLen + 1];
 
-        return allCardMatches(sLen, pLen, s, p, memoDP);		
+        return allCardMatches(sLen, pLen, s, p, memoDP);	
+        */	
 
         // Bottom Up Approach:
+        int sLen = s.length();
+        int pLen = p.length();
+
+        boolean[][] memoDP = new boolean[sLen + 1][pLen + 1];
+
+        // Base Case:
+        memoDP[0][0] = true;
+
+        for(int j = 1; j <= pLen; j++){
+            // String is empty, pattern must be all '*'
+            boolean allMatches = true;
+            for (int k = 0; k < j; k++) {
+                if (p.charAt(k) != '*'){
+                    allMatches = false;
+                    break;
+                }
+            }
+            memoDP[0][j] = allMatches;
+        }
+
+        for(int sIdx = 1; sIdx <= sLen; sIdx++){
+            for(int pIdx = 1; pIdx <= pLen; pIdx++){
+                boolean matchCase = false;
+                boolean notMatchCase = false;
+
+                if(s.charAt(sIdx - 1) == p.charAt(pIdx - 1)){
+                    matchCase = memoDP[sIdx - 1][pIdx - 1];
+                }
+                else if(p.charAt(pIdx - 1) == '?'){
+                    matchCase = memoDP[sIdx - 1][pIdx - 1] || matchCase;
+                }
+                else if(p.charAt(pIdx - 1) == '*'){
+                    boolean emptyCase = memoDP[sIdx][pIdx - 1];
+                    boolean seqCase = memoDP[sIdx - 1][pIdx];
+
+                    notMatchCase = emptyCase || seqCase;
+                }
+
+                memoDP[sIdx][pIdx] = matchCase || notMatchCase;
+            }
+        }
         
+        return memoDP[sLen][pLen];
     }
 }
