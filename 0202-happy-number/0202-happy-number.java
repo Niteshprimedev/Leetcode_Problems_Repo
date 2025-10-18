5bdbee30f75bc1ad8f779d7faecffc90482240e6
Nitesh Sharma <niteshprimedev@gmail.com>
2025-10-18 11:14:29 +0530

    Time: 2 ms (16.64%), Space: 40.7 MB (64.56%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isHappy(int n) {
+        TreeSet<Integer> seen = new TreeSet<>();
+
+        while(!seen.contains(n)){
+            int newDigit = 0;
+            seen.add(n);
+
+            while(n > 0){
+                int digit = n % 10;
+                newDigit += digit * digit;
+
+                n /= 10;
+            }
+
+            n = newDigit;
+        }
+
+        return n == 1;
+    }
+}
