9200a3cd29938ccbd6c545624e1d669b00e2c9d3
Nitesh Sharma <niteshprimedev@gmail.com>
2025-10-19 10:37:24 +0530

    Time: 1 ms (99.7%), Space: 44.2 MB (46.91%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,49 +1,29 @@
 class Solution {
-    int countDigits(int groupSize){
-        int totalDigits = 0;
-        if(groupSize == -1){
-            return totalDigits;
-        }
-
-        while (groupSize > 0){
-            totalDigits += 1;
-            groupSize /= 10;
-        }
-
-        return totalDigits;
-    }
-
     public int compress(char[] chars) {
-        int groupStrtIdx  = 0;
-        int groupEndIdx = 1;
-        int compressedStrIdx = 0;
+        int finalLen = 0;
+        int i = 0;
+        int n = chars.length;
 
-        for(; groupEndIdx <= chars.length; groupEndIdx++){
-            if(groupEndIdx == chars.length || chars[groupEndIdx] != chars[groupStrtIdx]){
-                int groupSize = groupEndIdx - groupStrtIdx;
+        while(i < n){
+            int freqCount = 1;
 
-                chars[compressedStrIdx++] = chars[groupStrtIdx];
-
-                if(groupSize > 1){
-                    int totalDigits = countDigits(groupSize);
-                    int idx = totalDigits;
-
-                    while(groupSize > 0){
-                        int currDigit = groupSize % 10;
-                        chars[compressedStrIdx + totalDigits - 1] = Integer.toString(currDigit).charAt(0);
-
-                        totalDigits -= 1;
-                        groupSize /= 10;
-                    }
-
-                    compressedStrIdx += idx;
-                }
-
-                groupStrtIdx = groupEndIdx;
+            while((i + 1) < n && chars[i] == chars[i + 1]){
+                freqCount += 1;
+                i += 1;
             }
 
+            chars[finalLen++] = chars[i];
+            i += 1;
+
+            if(freqCount <= 1) continue;
+
+            String revFreqCount = String.valueOf(freqCount);
+
+            for(char currCharDigit : revFreqCount.toCharArray()){
+                chars[finalLen++] = currCharDigit;
+            }
         }
 
-        return compressedStrIdx;
+        return finalLen;
     }
 }
