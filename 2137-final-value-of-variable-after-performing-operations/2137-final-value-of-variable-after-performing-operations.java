c28f6515915b7c56c47276f59de914ab3db44cd6
Nitesh Sharma <niteshprimedev@gmail.com>
2025-10-20 08:49:30 +0530

    Time: 3 ms (1.17%), Space: 43.2 MB (14.72%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -19,9 +19,29 @@ class Solution {
         return xValue.get();
         */
 
+        // Using Method Refs:
+        Optional<Integer> xValue = Arrays.stream(operations).map(currOp -> {
+            if(currOp.charAt(0) == '+'){
+                return 1;
+            }
+            else if(currOp.charAt(0) == '-'){
+                return -1;
+            }
+            else if(currOp.charAt(2) == '+'){
+                return 1;
+            }
+            else{
+                return -1;
+            }
+        }).reduce(Integer::sum);
+
+        return xValue.get();
+
+        /*
         // Performance Better Solution:
         int xValue = Arrays.stream(operations).mapToInt(currOp -> currOp.charAt(0) == '+' || currOp.charAt(2) == '+' ? 1 : -1).sum(); // Stream<String> to IntStream => then sum() method on primitives
 
         return xValue;
+        */
     }
 }
