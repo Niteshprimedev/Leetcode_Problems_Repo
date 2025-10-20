c3a6d8928b93707bab18159d619c837424bf34db
Nitesh Sharma <niteshprimedev@gmail.com>
2025-10-20 09:50:50 +0530

    Time: 3 ms (1.17%), Space: 43.1 MB (14.72%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -1,6 +1,12 @@
 class Solution {
+
+    // using map method ref;
+    private static int opValue(String currOp){
+        return currOp.charAt(0) == '+' || currOp.charAt(2) == '+' ? 1 : -1;
+    }
+
     public int finalValueAfterOperations(String[] operations) {
-        /*
+        /* Solution 1
         Optional<Integer> xValue = Arrays.stream(operations).map(currOp -> {
             if(currOp.charAt(0) == '+'){
                 return 1;
@@ -19,7 +25,7 @@ class Solution {
         return xValue.get();
         */
 
-        /*
+        /* Solution 2
         // Using Method Refs:
         Optional<Integer> xValue = Arrays.stream(operations).map(currOp -> {
             if(currOp.charAt(0) == '+'){
@@ -39,6 +45,7 @@ class Solution {
         return xValue.get();
         */
         
+        /* Solution 3
         // Using reduce Identity 0 => then no Optional<Integer> only int
         int xValue = Arrays.stream(operations).map(currOp -> {
             if(currOp.charAt(0) == '+'){
@@ -56,8 +63,15 @@ class Solution {
         }).reduce(0, Integer::sum);
 
         return xValue;
+        */
+        
+        // Solution 4
+        // Using reduce Identity 0 => then no Optional<Integer> only int
+        int xValue = Arrays.stream(operations).map(Solution::opValue).reduce(0, Integer::sum);
 
-        /*
+        return xValue;
+
+        /* Solution 5
         // Performance Better Solution:
         int xValue = Arrays.stream(operations).mapToInt(currOp -> currOp.charAt(0) == '+' || currOp.charAt(2) == '+' ? 1 : -1).sum(); // Stream<String> to IntStream => then sum() method on primitives
 
