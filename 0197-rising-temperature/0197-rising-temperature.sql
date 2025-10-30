1133f7f655e00a3b300136f93ee688c86dd9cecd
Nitesh Sharma <niteshprimedev@gmail.com>
2025-10-30 12:43:10 +0530

    Time: 432 ms (75.08%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -2,5 +2,5 @@
 SELECT w1.id AS Id
 FROM Weather AS w1
 JOIN Weather AS w2
-ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
-WHERE w1.temperature > w2.temperature;
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1 -- generates 16 rows for 4 rows table => 4 * 4
+WHERE w1.temperature > w2.temperature; -- filters the rows where temp1 > temp2
