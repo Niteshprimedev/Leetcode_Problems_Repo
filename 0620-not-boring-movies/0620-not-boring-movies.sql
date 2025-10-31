6df598ef0058e0a35c952817a835438fe0416314
Nitesh Sharma <niteshprimedev@gmail.com>
2025-10-31 20:59:30 +0530

    Time: 226 ms (62.55%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,5 +1,11 @@
 # Write your MySQL query statement below
-SELECT * 
+/*SELECT * 
 FROM Cinema
 WHERE MOD(id, 2) = 1 AND description <> 'boring'
+ORDER BY rating DESC;
+*/
+
+SELECT *
+FROM (SELECT * FROM Cinema WHERE id % 2 = 1) AS OddCinema
+WHERE description <> 'boring'
 ORDER BY rating DESC;
