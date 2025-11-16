af8569e012fefa13ac2779a6a35098830384b72c
Nitesh Sharma <niteshprimedev@gmail.com>
2025-11-16 19:37:08 +0530

    Time: 379 ms (38.08%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement below
+WITH Employee_With_Managers AS (
+    SELECT * FROM Employee WHERE managerId IS NOT NULL
+)
 SELECT e.name AS Employee
-FROM Employee AS e
+FROM Employee_With_Managers AS e
 INNER JOIN Employee AS m
 ON e.managerId = m.id
 AND e.salary > m.salary;
