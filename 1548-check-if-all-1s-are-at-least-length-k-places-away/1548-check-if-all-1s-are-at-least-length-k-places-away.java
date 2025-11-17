a1250f03f212aa533e1e3e30168b420d4faff552
Nitesh Sharma <niteshprimedev@gmail.com>
2025-11-17 10:05:21 +0530

    Time: 1 ms (99.71%), Space: 65.7 MB (5.71%) - LeetHub

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean kLengthApart(int[] nums, int k) {
+        /*
         int distance = -1;
         for(int num : nums){
             if(num == 1 && (distance == -1 || distance >= k)){
@@ -14,6 +15,18 @@ class Solution {
             // System.out.println("Hello from it: " + distance);
         }
 
+        return true;
+        */
+
+        int prev = (int) -1e9;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 1){
+                if(i - prev <= k) return false;
+                prev = i;
+            }
+        }
+        
         return true;
     }
 }
