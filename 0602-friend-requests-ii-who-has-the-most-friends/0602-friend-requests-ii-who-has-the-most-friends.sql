b603c228036920643dc02f145da0b97f81298775
Nitesh Sharma <niteshprimedev@gmail.com>
2026-01-11 19:14:02 +0530

    Time: 321 ms (20.37%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,11 +1,11 @@
 # Write your MySQL query statement below
+/*
 WITH TotalUsers AS
 (
     SELECT DISTINCT(requester_id) AS user_id FROM RequestAccepted
     UNION
     SELECT DISTINCT(accepter_id) AS user_id FROM RequestAccepted
 )
-
 SELECT 
     user_id AS id,
     total_friends AS num
@@ -18,4 +18,18 @@ FROM
     ON tu.user_id = ra.requester_id OR tu.user_id = ra.accepter_id
     GROUP BY tu.user_id) AS UsersFriends
 ORDER BY num DESC
+LIMIT 1;
+*/
+
+-- OR UNION ALL
+WITH TotalUsers AS 
+(
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id FROM RequestAccepted
+)
+SELECT id, COUNT(*) AS num
+FROM TotalUsers
+GROUP BY 1
+ORDER BY 2 DESC
 LIMIT 1;
