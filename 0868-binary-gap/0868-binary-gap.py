69f4f861b4aaf0f7ee4f306347bba0d4ad12dd45
Nitesh Sharma <niteshprimedev@gmail.com>
2026-02-22 18:10:08 +0530

    Time: 2 ms (12.15%), Space: 19.4 MB (31.55%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.py
+++ 0868-binary-gap/0868-binary-gap.py
@@ -1,6 +1,7 @@
 class Solution:
     def binaryGap(self, n: int) -> int:
         
+        '''
         max_bin_gap = 0
         prev_bit = -1
         gap_count = 0
@@ -20,3 +21,24 @@ class Solution:
             n = n >> 1
 
         return max_bin_gap
+        '''
+
+        # Counting Consecutive Zeroes;
+
+        n //= n & -n
+        if n == 1:
+            return 0
+            
+        max_gap = 0
+        gap = 0
+
+        while n:
+            if n & 1:
+                max_gap = max(max_gap, gap)
+                gap = 0
+            else:
+                gap += 1
+            
+            n >>= 1
+        
+        return max_gap + 1
