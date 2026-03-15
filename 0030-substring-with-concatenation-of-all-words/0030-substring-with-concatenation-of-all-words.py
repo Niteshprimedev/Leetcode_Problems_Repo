3827bda0cb78cbac9694e2119c943770523ff135
Nitesh Sharma <niteshprimedev@gmail.com>
2026-03-15 07:16:19 +0530

    Time: 9508 ms (5.01%), Space: 20 MB (35.65%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -52,6 +52,7 @@ class Solution:
         return concatenated_strings
         '''
 
+        # create a freq map of the words arr
         def frequency_map(word_list):
             freq = defaultdict(int)
 
@@ -69,24 +70,44 @@ class Solution:
         loop_len = len(s) - total_words_size + 1
 
         window_end_idx = 0
+
+        # start checking the substrings till last char before the total_words_size
         while window_end_idx < loop_len:
             idx = window_end_idx
             words_arr = []
 
+            # run a loop until the total_words_size from curr_idx
             while idx < (window_end_idx + total_words_size):
                 curr_word = s[idx: idx + word_size]
+                
+                # and check each word of word_size length to be in map
                 if curr_word in words_map:
+
+                    # if the word in map, this is potential a part of substring
                     curr_word = s[idx: idx + word_size]
                     words_arr.append(curr_word)
+                # else break the loop
                 else:
                     break     
 
                 idx += word_size          
 
+            # if the words_arr has same number of words then check freq;
             if len(words_arr) == len(words):
                 if frequency_map(words_arr) == words_map:
                     concatenated_strings.append(window_end_idx)
 
+            # continue checking with the next char;
             window_end_idx += 1
         
         return concatenated_strings
+
+
+        # barfoothefoobarman
+        # barfoo, arfoot, foothe, thefoo, foobar, barman
+        # bar, foo => freq (1, 1) => matched => pushed hence 0 ans
+        # arf => not in map break, not same length go to next char check
+        # foo (in), the not in map => break, not same length
+        # the => not in map break
+        # foo, bar => freq (1, 1) => matches => pushed & next hence 9 ans
+        # bar, man (not in map), break and done with iteration;
