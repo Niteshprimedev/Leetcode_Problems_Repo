cc89ad8266cd324108f6e7f5d6a8044178541ad8
Nitesh Sharma <niteshprimedev@gmail.com>
2026-03-18 07:48:11 +0530

    Time: 52 ms (81.96%), Space: 67 MB (92.5%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,100 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        // Solution 1 - Using Without Nested while Loop
+        int maxFruitsCount = 0;
+        int strtIdx = 0;
+        int endIdx = 0;
+        HashMap<Integer, Integer> fruitsTyepeFreq= new HashMap<>();
+
+        while(endIdx < fruits.length){
+            int currFruit = fruits[endIdx];
+            int hashValue = fruitsTyepeFreq.get(currFruit) == null ? 1 : fruitsTyepeFreq.get(currFruit) + 1;
+
+            fruitsTyepeFreq.put(currFruit, hashValue);
+
+            if(fruitsTyepeFreq.size() > 2){
+                int strtFruit = fruits[strtIdx];
+                hashValue = fruitsTyepeFreq.get(strtFruit);
+
+                if(hashValue == 1){
+                    fruitsTyepeFreq.remove(strtFruit);
+                }
+                else{
+                    fruitsTyepeFreq.put(strtFruit, hashValue - 1);
+                }
+                
+                strtIdx += 1;
+            }
+            
+            maxFruitsCount = Math.max(maxFruitsCount, (endIdx - strtIdx + 1));
+            endIdx += 1;
+        }
+
+        return maxFruitsCount;
+
+        /*
+        // Solution 2 - Using while Nested Loop
+        int maxFruitsCount = 0;
+        int strtIdx = 0;
+        int endIdx = 0;
+        HashMap<Integer, Integer> fruitsTyepeFreq= new HashMap<>();
+
+        while(endIdx < fruits.length){
+            int currFruit = fruits[endIdx];
+            int hashValue = fruitsTyepeFreq.get(currFruit) == null ? 1 : fruitsTyepeFreq.get(currFruit) + 1;
+
+            fruitsTyepeFreq.put(currFruit, hashValue);
+
+            while(fruitsTyepeFreq.size() > 2 && strtIdx <= endIdx){
+                int strtFruit = fruits[strtIdx];
+                hashValue = fruitsTyepeFreq.get(strtFruit);
+
+                if(hashValue == 1){
+                    fruitsTyepeFreq.remove(strtFruit);
+                }
+                else{
+                    fruitsTyepeFreq.put(strtFruit, hashValue - 1);
+                }
+                
+                strtIdx += 1;
+            }
+            
+            maxFruitsCount = Math.max(maxFruitsCount, (endIdx - strtIdx + 1));
+            endIdx += 1;
+        }
+
+        return maxFruitsCount;
+        */
+
+        /*
+        // Solution 3: Using Variables & No Map;
+        int fruitTypeFirst = -1;
+        int fruitTypeSecond = -1;
+
+        int fruitTypeFirstFreq = 0;
+        int fruitTypeSecondFreq = 0;
+
+        for(int fruit : fruits){
+             if(fruit == fruitTypeFirst){
+                fruitTypeFirstFreq += 1;
+            }
+            
+            if(fruit == fruitTypeSecond){
+                fruitTypeSecondFreq += 1;
+            }
+            
+            if(fruitTypeFirst == -1){
+                fruitTypeFirst = fruit;
+                fruitTypeFirstFreq += 1;
+            }
+            else if(fruitTypeSecond == -1){
+                fruitTypeSecond = fruit;
+                fruitTypeSecondFreq += 1;
+            }
+            else{
+
+            }
+        }
+        */
+    }
+}
