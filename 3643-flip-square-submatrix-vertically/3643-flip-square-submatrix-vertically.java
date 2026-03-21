57d33806477d787754d5ed4a112bd4e3afd7053b
Nitesh Sharma <niteshprimedev@gmail.com>
2026-03-21 09:49:45 +0530

    Time: 0 ms (100%), Space: 47.3 MB (78.37%) - LeetHub

--- 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.java
+++ 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+        /*
         // Use Transpose Technique:
         int endRowIdx = 0;
 
@@ -13,6 +14,26 @@ class Solution {
                 grid[endRow][colIdx] = strtRowVal;
                 
                 strtRow += 1;
+            }
+        }
+
+        return grid;
+        */
+
+        // Another solution => Clean Code;
+        int endRowIdx = x + k;
+        int endColIdx = y + k;
+
+        for(int colIdx = y; colIdx < endColIdx; colIdx++){
+            int endRow = endRowIdx - 1;
+            for(int strtRow = x; strtRow < endRowIdx; strtRow++){
+                if(strtRow >= endRow) break;
+
+                int strtRowVal = grid[strtRow][colIdx];
+                grid[strtRow][colIdx] = grid[endRow][colIdx];
+                grid[endRow][colIdx] = strtRowVal;
+
+                endRow -= 1;
             }
         }
 
