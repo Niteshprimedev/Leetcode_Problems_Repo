1730cbeb2c7ad3045d067516c09c134696bc3d12
Nitesh Sharma <niteshprimedev@gmail.com>
2026-03-23 09:29:34 +0530

    Time: 1 ms (0%), Space: 45 MB (100%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        boolean isEven = false;
+        boolean isOdd = false;
+
+        for(int num : nums1){
+            if(num % 2 == 0){
+                isEven = true;
+            }
+            else{
+                isOdd = true;
+            }
+        }
+
+        if(isEven && !isOdd) return true;
+        if(!isEven && isOdd) return true;
+
+        // now thinking:
+        // even & odd -> odd
+        // 2 - 5 => -3
+        // even all => even
+        // odd all => odd
+        // but even & odd => we want odd case only;
+        int oddNum = -1;
+
+        for(int num : nums1){
+            if(num % 2 == 1){
+                oddNum = num;
+            }
+        }
+
+        int[] nums2 = new int[nums1.length];
+
+        for(int i = 0; i < nums1.length; i++){
+            nums2[i] = nums1[i];
+
+            if(nums1[i] % 2 == 0){
+                nums2[i] = nums1[i] - oddNum;
+            }
+        }
+
+        for(int num : nums2){
+            if(num % 2 == 0) return false;
+        }
+
+        return true;
+    }
+}
