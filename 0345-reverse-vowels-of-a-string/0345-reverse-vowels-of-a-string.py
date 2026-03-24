bd01ca93ead45ed50181c09da17853b07f118181
Nitesh Sharma <niteshprimedev@gmail.com>
2026-03-24 09:50:53 +0530

    Time: 13 ms (39.12%), Space: 20.5 MB (69.33%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,5 +1,6 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
+        '''
         VOWELS = set('aeiou')
 
         chars_list = list(s)
@@ -24,6 +25,34 @@ class Solution:
 
             left_idx += 1
             right_idx -= 1
+            
+            # print(left_idx, right_idx, chars_list)
+            
+        return ''.join(chars_list)
+        '''
+
+        # Meta Prep Time Practice
+        # Solution using Conditionals and While loop;
+        VOWELS = set('aeiou')
+
+        chars_list = list(s)
+
+        left_idx = 0
+        right_idx = len(chars_list) - 1
+
+        while(left_idx < right_idx):
+            left_char = chars_list[left_idx]
+            right_char = chars_list[right_idx]
+
+            if left_char.lower() not in VOWELS:
+                left_idx += 1
+            elif right_char.lower() not in VOWELS:
+                right_idx -= 1
+            else:
+                # Swap the Vowels: at left_idx & right_idx
+                chars_list[left_idx], chars_list[right_idx] = chars_list[right_idx], chars_list[left_idx]
+                left_idx += 1
+                right_idx -= 1
             
             # print(left_idx, right_idx, chars_list)
             
