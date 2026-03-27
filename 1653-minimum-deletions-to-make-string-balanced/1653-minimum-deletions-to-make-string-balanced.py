da5053a1c7097cf9f4f5dd7f4cfbf20bbe48662a
Nitesh Sharma <niteshprimedev@gmail.com>
2026-03-27 10:34:25 +0530

    Time: 507 ms (16.44%), Space: 24.9 MB (16.55%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -31,3 +31,45 @@ class Solution:
         for i in range(n):
             min_deletions = min(min_deletions, count_a[i] + count_b[i])
         return min_deletions
+
+
+        '''
+        # Meta Prep Time Practice
+        # Solution using a_count_pos & b_count_pos
+        # and the min freq of the chars a or b
+
+        n = len(s)
+        b_char_first_pos = -1
+        a_char_last_pos = n
+
+        for i in range(n):
+            if b_char_first_pos == -1 and s[i] == 'b':
+                b_char_first_pos = i
+            elif a_char_last_pos == n and s[n - 1 - i] == 'a':
+                a_char_last_pos = n - 1 - i
+            
+            if b_char_first_pos != -1 and a_char_last_pos != n:
+                break
+        
+        # Edge case: if only all a chars or all b chars then 0
+        if b_char_first_pos == -1 or a_char_last_pos == n:
+            return 0
+
+        a_count = 0
+        b_count = 0
+
+        print(b_char_first_pos, a_char_last_pos, len(s))
+        strt = b_char_first_pos
+
+        while(strt <= a_char_last_pos):
+            if s[strt] == 'b':
+                b_count += 1
+            
+            if s[strt] == 'a':
+                a_count += 1
+            
+            strt += 1
+
+        min_deletions = min(a_count, b_count)
+        return min_deletions
+        '''
