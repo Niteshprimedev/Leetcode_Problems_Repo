5fd2840861f545042538178bdcfa69600579d521
Nitesh Sharma <niteshprimedev@gmail.com>
2026-04-07 09:58:24 +0530

    Time: 63 ms (18.04%), Space: 120.7 MB (80.69%) - LeetHub

---
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -0,0 +1,52 @@
+class Solution {
+    class Element{
+        int sum = 0;
+        int idxI = 0;
+        int idxJ = 0;
+
+        public Element(int sum, int idxI, int idxJ){
+            this.sum = sum;
+            this.idxI = idxI;
+            this.idxJ = idxJ;
+        }
+    }
+
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        List<List<Integer>> smallestPairs = new ArrayList<>();
+        int idxI = 0;
+        int idxJ = 0;
+        int sumCount = k;
+
+        Set<Pair> visitedPairs = new HashSet<>();
+
+        PriorityQueue<Element> minHeap = new PriorityQueue<>((el1, el2) -> el1.sum - el2.sum);
+        minHeap.add(new Element(nums1[idxI] + nums2[idxJ], idxI, idxJ));
+        visitedPairs.add(new Pair<>(idxI, idxJ));
+
+        while(sumCount > 0 && !minHeap.isEmpty()){
+            Element currElement = minHeap.poll();
+
+            int currSum = currElement.sum;
+            idxI = currElement.idxI;
+            idxJ = currElement.idxJ;
+
+            smallestPairs.add(Arrays.asList(nums1[idxI], nums2[idxJ]));
+
+            if(idxI + 1 < m && !visitedPairs.contains(new Pair<>(idxI + 1, idxJ))){
+                minHeap.add(new Element(nums1[idxI + 1] + nums2[idxJ], idxI + 1, idxJ));
+                visitedPairs.add(new Pair<>(idxI + 1, idxJ));
+            }
+            if(idxJ + 1 < n && !visitedPairs.contains(new Pair<>(idxI, idxJ + 1))){
+                minHeap.add(new Element(nums1[idxI] + nums2[idxJ + 1], idxI, idxJ + 1));
+                visitedPairs.add(new Pair<>(idxI, idxJ + 1));
+            }
+
+            sumCount -= 1;
+        }
+
+        return smallestPairs;
+    }
+}
