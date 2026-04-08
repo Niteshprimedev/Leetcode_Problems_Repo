e1fda35e305ffa5e2d9bf35d73d185849345185c
Nitesh Sharma <niteshprimedev@gmail.com>
2026-04-08 11:25:17 +0530

    Time: 78 ms (12.57%), Space: 158.2 MB (9.68%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,33 +1,34 @@
-class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
+class Pair{
+    public int val;
+    public int idx;
+
+    public Pair(int val, int idx) {
+        this.val = val;
+        this.idx = idx;
+    }
+};
+
+class Solution { 
+    // 8 6 4 12 3 2 1 1 , k=3 || max = 8 , 12 , 12  , 12 ,  3 ,2 
+    // i = 7  
+    // pq = (8,0) (6,1) (4,2) => (12,3) (6,1) (4,2) (3,4) (2,5) => (3,4) (2,5) (1,6) => (2,5) (1,6) (1,7) 
+    // while(!pq.isEmpty() && i-pq.peek()[1]>=k) {pq.poll();}
+
+
+    public int[] maxSlidingWindow(int[] nums, int k) { 
         int n = nums.length;
-		int[] slidingWindowMaxs = new int[n - k + 1];
-		Deque <Integer> queue = new ArrayDeque<>();
-		
-		int strtIdx = 0;
-		int endIdx = 0;
-		
-		while (endIdx < nums.length){
-			int currNum = nums[endIdx];
-			
-			while(!queue.isEmpty() && nums[queue.getLast()] <= currNum){
-				queue.pollLast();
-			}
-			
-			while(!queue.isEmpty() && (endIdx - queue.getFirst()) >= k){
-				queue.pollFirst();
-			}
-			
-			queue.offerLast(endIdx);
-			
-			if(endIdx - strtIdx + 1 == k){
-				slidingWindowMaxs[strtIdx] = nums[queue.getFirst()];
-				strtIdx += 1;
-			}
-			
-			endIdx += 1;
-		}
-		
-		return slidingWindowMaxs;
+        int[] ans = new int[n - k + 1];
+        PriorityQueue<Pair> pq = new PriorityQueue<>((p1, p2) -> p2.val - p1.val); // desc sorting
+        for(int i=0;i<nums.length;i++){
+            pq.add(new Pair(nums[i],i));
+            while(!pq.isEmpty() && i-pq.peek().idx >=k ){
+                pq.poll();
+            }
+            if(i>=k-1){
+                ans[i-k+1] = pq.peek().val;
+            }
+            // System.out.println(Arrays.toString(ans));
+        }
+        return ans;
     }
 }
