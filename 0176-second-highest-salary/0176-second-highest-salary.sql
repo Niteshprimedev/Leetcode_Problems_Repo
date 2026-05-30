74bbdb43036ea50ae0a812ce8993d1a58ce91e02
Nitesh Sharma <niteshprimedev@gmail.com>
2026-05-30 20:42:01 +0530

    Time: 275 ms (65.52%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,26 @@
 # Write your MySQL query statement below
+/*
 WITH EmployeesWithMaxSalary AS (
     SELECT MAX(salary) AS max_salary FROM Employee
 )
 SELECT IFNULL(MAX(salary), null) AS SecondHighestSalary
 FROM Employee
 WHERE salary < (SELECT max_salary FROM EmployeesWithMaxSalary);
+*/
+
+-- OR
+
+/*
+SELECT MAX(salary) OVER(ORDER BY salary DESC OFFSET 1) AS SecondHighestSalary
+FROM Employee
+LIMIT 1;
+*/
+
+WITH rankedSalaries AS (
+    SELECT salary,
+DENSE_RANK() OVER(ORDER BY salary DESC) AS salaried_rank
+FROM Employee
+)
+SELECT IFNULL(MAX(salary), null) AS SecondHighestSalary
+FROM rankedSalaries
+WHERE salaried_rank = 2;
