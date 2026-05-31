cf75d0673789072b41bbc6483120340d89e92a9a
Nitesh Sharma <niteshprimedev@gmail.com>
2026-05-31 22:59:59 +0530

    Time: 25 ms (93.3%), Space: 107.5 MB (47.81%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        /*
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a, b));
+
+        for(int asteroid : asteroids){
+            minHeap.add(asteroid);
+        }
+
+        int planetMass = mass;
+
+        while(!minHeap.isEmpty()){
+            int currAsteroid = minHeap.poll();
+
+            if(planetMass >= currAsteroid){
+                planetMass += currAsteroid;
+            }
+            else{
+                return false;
+            }
+        }
+
+        return true;
+        */
+
+        Arrays.sort(asteroids);
+
+        long planetMass = mass;
+
+        for(int currAsteroid : asteroids){
+            if(planetMass >= (long) currAsteroid){
+                planetMass += currAsteroid;
+            }
+            else{
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
