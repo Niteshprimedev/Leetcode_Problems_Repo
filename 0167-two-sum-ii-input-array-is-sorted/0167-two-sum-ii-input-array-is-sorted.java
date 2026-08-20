87d60625029b9e3b9dd2d9cc72ceb1f81d493eed
Nitesh Sharma <niteshprimedev@gmail.com>
2026-08-20 09:51:26 +0530

    Time: 5 ms (13.43%), Space: 49 MB (5.03%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,9 +1,13 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
+        /*
+        // Meta Prep Time for April Hiring;
+        // Optimal Solution 1
         int strt = 0;
         int end = numbers.length - 1;
         int[] result = new int[2];
 
+        // Time Complexity -> O(N); & SC -> O(1)
         while(strt < end){
             if(numbers[strt] + numbers[end] == target){
                 result[0] = strt + 1;
@@ -19,5 +23,23 @@ class Solution {
         }
 
         return result;
+        */
+
+        // SubOptimal Solution:
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = numbers.length;
+        
+        for(int i = 0; i < n; i++){
+            int currNum = numbers[i];
+            int secondNum = target - currNum;
+
+            if(map.containsKey(secondNum)){
+                return new int[]{map.get(secondNum) + 1, i + 1};
+            }
+
+            map.put(currNum, i);
+        }
+
+        return new int[]{};
     }
 }
